1d7275df3feebe1ca8c71fc268682119bd621771
dkavalets <dzmitry_kavalets@epam.com>
2016-11-24 11:40:58 +0300

    Fix links

--- src/main/java/com/epam/ta/reportportal/triggers/DeleteProjectListener.java
+++ src/main/java/com/epam/ta/reportportal/triggers/DeleteProjectListener.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2016 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/commons-dao
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.epam.ta.reportportal.triggers;
 
 import static com.epam.ta.reportportal.database.personal.PersonalProjectUtils.personalProjectName;
@@ -24,6 +44,9 @@ import com.epam.ta.reportportal.database.entity.widget.Widget;
 import com.epam.ta.reportportal.database.support.RepositoryProvider;
 import com.mongodb.DBObject;
 
+/**
+ * @author Dzmitry Kavalets
+ */
 @Component
 public class DeleteProjectListener extends AbstractMongoEventListener<Project> {
 
--- src/test/java/com/epam/ta/reportportal/triggers/DeleteProjectListenerTest.java
+++ src/test/java/com/epam/ta/reportportal/triggers/DeleteProjectListenerTest.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2016 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/commons-dao
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.epam.ta.reportportal.triggers;
 
 import static java.util.Collections.singletonList;
@@ -20,6 +40,9 @@ import com.epam.ta.reportportal.database.entity.*;
 import com.epam.ta.reportportal.database.entity.item.Activity;
 import com.epam.ta.reportportal.database.entity.item.TestItem;
 
+/**
+ * @author Dzmitry Kavalets
+ */
 public class DeleteProjectListenerTest extends BaseDaoTest {
 
 	public static final String CONTENT_TYPE = "image/png";
