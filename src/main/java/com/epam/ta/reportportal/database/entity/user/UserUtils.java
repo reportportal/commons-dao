a7e17c76d00abd6832d97aec958c2ae9109fffc4
Andrei Varabyeu <andrei_varabyeu@epam.com>
2017-02-22 15:52:29 +0300

    add utility method to user utils

--- src/main/java/com/epam/ta/reportportal/database/entity/user/UserUtils.java
+++ src/main/java/com/epam/ta/reportportal/database/entity/user/UserUtils.java
@@ -17,55 +17,75 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 
 package com.epam.ta.reportportal.database.entity.user;
+
+import com.epam.ta.reportportal.database.entity.Project;
+import com.epam.ta.reportportal.database.entity.ProjectRole;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
-import org.apache.commons.validator.routines.EmailValidator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * User entry related utilities
- * 
+ *
  * @author Andrei_Ramanchuk
  */
 public class UserUtils {
 
-	/**
-	 * MD5 password hashing
-	 * 
-	 * @param initial
-	 * @return
-	 */
-	public static String generateMD5(String initial) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			byte[] byteInitial = initial.getBytes("UTF-8");
-			byteInitial = md.digest(byteInitial);
-			// convert the byte to hex format
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < byteInitial.length; i++) {
-				sb.append(Integer.toString((byteInitial[i] & 0xff) + 0x100, 16).substring(1));
-			}
-			return sb.toString();
-		} catch (NoSuchAlgorithmException ex) {
-			throw new RuntimeException("Unable apply MD5 algorithm for password hashing: ", ex);
-		} catch (UnsupportedEncodingException unsEx) {
-			throw new RuntimeException("Unable apply UTF-8 encoding for password string: ", unsEx);
-		}
-	}
+    /**
+     * MD5 password hashing
+     *
+     * @param initial
+     * @return
+     */
+    public static String generateMD5(String initial) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] byteInitial = initial.getBytes("UTF-8");
+            byteInitial = md.digest(byteInitial);
+            // convert the byte to hex format
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteInitial.length; i++) {
+                sb.append(Integer.toString((byteInitial[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException("Unable apply MD5 algorithm for password hashing: ", ex);
+        } catch (UnsupportedEncodingException unsEx) {
+            throw new RuntimeException("Unable apply UTF-8 encoding for password string: ", unsEx);
+        }
+    }
 
-	/**
-	 * Validate email format against RFC822
-	 * 
-	 * @param email
-	 * @return
-	 */
-	public static boolean isEmailValid(String email) {
-		EmailValidator validator = EmailValidator.getInstance();
-		return validator.isValid(email);
-	}
+    /**
+     * Validate email format against RFC822
+     *
+     * @param email
+     * @return
+     */
+    public static boolean isEmailValid(String email) {
+        EmailValidator validator = EmailValidator.getInstance();
+        return validator.isValid(email);
+    }
+
+    /**
+     * Transforms to map Project->ProjectRole
+     *
+     * @param user     Username
+     * @param projects List of assigned projects
+     * @return Project->ProjectRole map
+     */
+    public static Map<String, ProjectRole> getUserRoles(String user, List<Project> projects) {
+        return projects
+                .stream()
+                .filter(p -> p.getUsers().containsKey(user))
+                .collect(Collectors.
+                        toMap(Project::getName, p -> p.getUsers().get(user).getProjectRole()));
+    }
 }
