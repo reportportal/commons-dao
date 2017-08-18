ffbe672d64d7241319bf64fd29ffa058682331ce
pbortnik <pavel_bortnik@epam.com>
2017-08-18 23:52:02 +0300

    Update user config builder. Added user config getter for specified login

--- src/main/java/com/epam/ta/reportportal/database/entity/Project.java
+++ src/main/java/com/epam/ta/reportportal/database/entity/Project.java
@@ -26,6 +26,8 @@ import com.epam.ta.reportportal.database.entity.project.EntryType;
 import com.epam.ta.reportportal.database.entity.project.email.ProjectEmailConfig;
 import com.epam.ta.reportportal.database.entity.statistics.StatisticSubType;
 import com.epam.ta.reportportal.database.search.FilterCriteria;
+import com.epam.ta.reportportal.exception.ReportPortalException;
+import com.epam.ta.reportportal.ws.model.ErrorType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -142,6 +144,18 @@ public class Project implements Serializable {
 
     public void setMetadata(Metadata metadata) {
         this.metadata = metadata;
+    }
+
+    /**
+     * Finds UserConfig for specified login. Returns null
+     * if it doesn't exists.
+     *
+     * @param login Login for search
+     * @return UserConfig for specified login
+     */
+    public UserConfig getUserByLogin(String login) {
+        return users.stream().filter(it -> login.equals(it.getLogin())).findAny()
+                .orElseThrow(() -> new ReportPortalException(ErrorType.USER_NOT_FOUND, login));
     }
 
     @Override
@@ -375,6 +389,11 @@ public class Project implements Serializable {
 
         public UserConfig withProjectRole(ProjectRole projectRole) {
             this.projectRole = projectRole;
+            return this;
+        }
+
+        public UserConfig withLogin(String login) {
+            this.login = login;
             return this;
         }
     }
