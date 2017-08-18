11012334897efe3e84cd148c7f4b240b4a3cd782
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-08-18 14:27:40 +0300

    introduce activities
    Signed-off-by: Andrei Varabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/database/CustomMongoConverters.java
+++ src/main/java/com/epam/ta/reportportal/database/CustomMongoConverters.java
@@ -25,17 +25,44 @@ import com.epam.ta.reportportal.database.entity.item.Activity;
 import com.epam.ta.reportportal.database.entity.item.ActivityEventType;
 import com.epam.ta.reportportal.database.entity.item.ActivityObjectType;
 import com.epam.ta.reportportal.ws.model.ErrorType;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.ConditionalConverter;
 import org.springframework.core.convert.converter.Converter;
 
 import com.epam.ta.reportportal.database.entity.LogLevel;
 import com.epam.ta.reportportal.exception.ReportPortalException;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
+import org.springframework.util.ClassUtils;
 
 public class CustomMongoConverters {
 
 	private CustomMongoConverters() {
 		//statics only
+	}
+
+	class InternalAbstractConditionalEnumConverter implements ConditionalConverter {
+
+		private final ConversionService conversionService;
+
+
+		protected InternalAbstractConditionalEnumConverter(ConversionService conversionService) {
+			this.conversionService = conversionService;
+		}
+
+		@Override
+		public boolean matches(TypeDescriptor sourceType, TypeDescriptor targetType) {
+			for (Class<?> interfaceType : ClassUtils.getAllInterfacesForClass(sourceType.getType())) {
+				if (this.conversionService.canConvert(TypeDescriptor.valueOf(interfaceType), targetType)) {
+					return false;
+				}
+			}
+			return true;
+		}
+
 	}
 
 	public enum LogLevelToIntConverter implements Converter<LogLevel, DBObject> {
@@ -96,6 +123,7 @@ public class CustomMongoConverters {
 		}
 	}
 
+	@ReadingConverter
 	public enum StringToActivityEventTypeConverter implements Converter<String, ActivityEventType> {
 		INSTANCE;
 
@@ -115,6 +143,7 @@ public class CustomMongoConverters {
 		}
 	}
 
+	@ReadingConverter
 	public enum StringToActivityObjectTypeConverter implements Converter<String, ActivityObjectType> {
 		INSTANCE;
 
