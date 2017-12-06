82ddf5ac5e1cca734f07da21939c31b5e5cd9384
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-12-06 22:13:50 +0300

    upd cascade deletion

--- src/main/java/com/epam/ta/reportportal/triggers/DeleteItemsListener.java
+++ src/main/java/com/epam/ta/reportportal/triggers/DeleteItemsListener.java
@@ -20,12 +20,11 @@
  */
 package com.epam.ta.reportportal.triggers;
 
-import static java.util.Collections.singletonList;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.StreamSupport.stream;
-
-import java.util.List;
-
+import com.epam.ta.reportportal.database.dao.LogRepository;
+import com.epam.ta.reportportal.database.entity.item.TestItem;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.QueryMapper;
@@ -34,10 +33,11 @@ import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventLis
 import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
 import org.springframework.stereotype.Service;
 
-import com.epam.ta.reportportal.database.dao.LogRepository;
-import com.epam.ta.reportportal.database.entity.item.TestItem;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
 
 /**
  * @author Dzmitry Kavalets
@@ -62,14 +62,17 @@ public class DeleteItemsListener extends AbstractMongoEventListener<TestItem> {
 	public void onBeforeDelete(BeforeDeleteEvent<TestItem> event) {
 		DBObject dbqo = queryMapper.getMappedObject(event.getDBObject(), mappingContext.getPersistentEntity(TestItem.class));
 		for (DBObject dbObject : mongoTemplate.getCollection(event.getCollectionName()).find(dbqo)) {
-			final String id = dbObject.get("_id").toString();
-			final BasicDBObject itemDescendantsQuery = new BasicDBObject("path", new BasicDBObject("$in", singletonList(id)));
-			final List<String> itemIds = stream(
-					mongoTemplate.getCollection(event.getCollectionName()).find(itemDescendantsQuery).spliterator(), false)
-							.map(it -> it.get("_id").toString()).collect(toList());
-			mongoTemplate.getCollection(event.getCollectionName()).remove(itemDescendantsQuery);
-			itemIds.add(id);
-			logRepository.deleteByItemRef(itemIds);
+			String isRetry = (String) dbObject.get("isRetry");
+			if (!BooleanUtils.toBoolean(isRetry)) {
+				final String id = dbObject.get("_id").toString();
+				final BasicDBObject itemDescendantsQuery = new BasicDBObject("path", new BasicDBObject("$in", singletonList(id)));
+				final List<String> itemIds = stream(
+						mongoTemplate.getCollection(event.getCollectionName()).find(itemDescendantsQuery).spliterator(), false).map(
+						it -> it.get("_id").toString()).collect(toList());
+				mongoTemplate.getCollection(event.getCollectionName()).remove(itemDescendantsQuery);
+				itemIds.add(id);
+				logRepository.deleteByItemRef(itemIds);
+			}
 		}
 	}
 }
