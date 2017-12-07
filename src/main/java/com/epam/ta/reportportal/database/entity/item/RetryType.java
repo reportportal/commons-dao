9cba413f72a6144f4b12da6621aef3436c614fa1
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-12-07 14:50:48 +0300

    update trigger on deleting

--- src/main/java/com/epam/ta/reportportal/database/entity/item/RetryType.java
+++ src/main/java/com/epam/ta/reportportal/database/entity/item/RetryType.java
@@ -1,12 +1,26 @@
 package com.epam.ta.reportportal.database.entity.item;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
  * @author Pavel Bortnik
  */
 public enum RetryType {
+	ROOT("root"),
+	RETRY("retry"),
+	LAST("last");
 
-	ROOT,
-	RETRY,
-	LAST
+	RetryType(String value) {
+		this.value = value;
+	}
+
+	private String value;
+
+	public static Optional<RetryType> fromString(String string) {
+		return Optional.ofNullable(string)
+				.flatMap(str -> Arrays.stream(values())
+						.filter(it -> it.value.equals(str)).findAny());
+	}
 
 }
--- src/main/java/com/epam/ta/reportportal/triggers/DeleteItemsListener.java
+++ src/main/java/com/epam/ta/reportportal/triggers/DeleteItemsListener.java
@@ -21,10 +21,10 @@
 package com.epam.ta.reportportal.triggers;
 
 import com.epam.ta.reportportal.database.dao.LogRepository;
+import com.epam.ta.reportportal.database.entity.item.RetryType;
 import com.epam.ta.reportportal.database.entity.item.TestItem;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.QueryMapper;
@@ -34,6 +34,7 @@ import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
@@ -62,17 +63,24 @@ public class DeleteItemsListener extends AbstractMongoEventListener<TestItem> {
 	public void onBeforeDelete(BeforeDeleteEvent<TestItem> event) {
 		DBObject dbqo = queryMapper.getMappedObject(event.getDBObject(), mappingContext.getPersistentEntity(TestItem.class));
 		for (DBObject dbObject : mongoTemplate.getCollection(event.getCollectionName()).find(dbqo)) {
-			Boolean isRetry = (Boolean) dbObject.get("isRetry");
-			if (BooleanUtils.isFalse(isRetry)) {
+			Optional<RetryType> retryType = RetryType.fromString((String) dbObject.get("retryType"));
+			if (retryType.isPresent() && retryType.get().equals(RetryType.LAST)) {
+				List<TestItem> retries = (List<TestItem>) dbObject.get("retries");
+				retries.stream().forEach(it -> clearItemData(event.getCollectionName(), it.getId()));
+			} else {
 				final String id = dbObject.get("_id").toString();
-				final BasicDBObject itemDescendantsQuery = new BasicDBObject("path", new BasicDBObject("$in", singletonList(id)));
-				final List<String> itemIds = stream(
-						mongoTemplate.getCollection(event.getCollectionName()).find(itemDescendantsQuery).spliterator(), false).map(
-						it -> it.get("_id").toString()).collect(toList());
-				mongoTemplate.getCollection(event.getCollectionName()).remove(itemDescendantsQuery);
-				itemIds.add(id);
-				logRepository.deleteByItemRef(itemIds);
+				clearItemData(event.getCollectionName(), id);
 			}
 		}
+	}
+
+	private void clearItemData(String collectionName, String id) {
+		final BasicDBObject itemDescendantsQuery = new BasicDBObject("path", new BasicDBObject("$in", singletonList(id)));
+		final List<String> itemIds = stream(
+				mongoTemplate.getCollection(collectionName).find(itemDescendantsQuery).spliterator(), false).map(
+				it -> it.get("_id").toString()).collect(toList());
+		mongoTemplate.getCollection(collectionName).remove(itemDescendantsQuery);
+		itemIds.add(id);
+		logRepository.deleteByItemRef(itemIds);
 	}
 }
