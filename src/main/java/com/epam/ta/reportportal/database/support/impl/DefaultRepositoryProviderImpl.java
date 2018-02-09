1b7e82c717a543974f54f562a7d70af74158ffa5
avarabyeu <andrei.varabyeu@gmail.com>
2018-02-09 15:47:25 +0300

    cleanup mongo's stuff
    Signed-off-by: avarabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/database/support/impl/DefaultRepositoryProviderImpl.java
+++ src/main/java/com/epam/ta/reportportal/database/support/impl/DefaultRepositoryProviderImpl.java
@@ -1,37 +1,36 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/commons-dao
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
- 
+ */
+
 package com.epam.ta.reportportal.database.support.impl;
 
+import com.epam.ta.reportportal.database.support.RepositoryProvider;
+import com.epam.ta.reportportal.exception.ReportPortalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.support.Repositories;
 
-import com.epam.ta.reportportal.database.support.RepositoryProvider;
-
 /**
  * Default implementation of {@link RepositoryProvider}
- * 
+ *
  * @author Andrei Varabyeu
- * 
  */
 public class DefaultRepositoryProviderImpl implements RepositoryProvider {
 
@@ -46,7 +45,8 @@ public class DefaultRepositoryProviderImpl implements RepositoryProvider {
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> PagingAndSortingRepository<T, ?> getRepositoryFor(Class<?> clazz) {
-		return (PagingAndSortingRepository<T, ?>) repositories.getRepositoryFor(clazz);
+		return (PagingAndSortingRepository<T, ?>) repositories.getRepositoryFor(clazz)
+				.orElseThrow(() -> new ReportPortalException("Repository not found"));
 	}
 
 }
