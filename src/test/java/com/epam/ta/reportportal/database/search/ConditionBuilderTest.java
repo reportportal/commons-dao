8badbe943b678326b9db1c0d15761b864acdceb0
Andrei Varabyeu <andrei_varabyeu@epam.com>
2018-02-14 01:46:48 +0300

    fix unit test

--- src/test/java/com/epam/ta/reportportal/database/search/ConditionBuilderTest.java
+++ src/test/java/com/epam/ta/reportportal/database/search/ConditionBuilderTest.java
@@ -25,8 +25,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.data.mongodb.core.query.Criteria;
 
-import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 import static org.hamcrest.Matchers.is;
@@ -38,27 +38,25 @@ public class ConditionBuilderTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void checkEmptyValue() {
-		FilterCondition.builder()
-				.withCondition(Condition.EQUALS)
-				.withSearchCriteria("xxx").build();
+		FilterCondition.builder().withCondition(Condition.EQUALS).withSearchCriteria("xxx").build();
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void checkEmptyCondition() {
-		FilterCondition.builder()
-				.withValue("hello")
-				.withSearchCriteria("xxx").build();
+		FilterCondition.builder().withValue("hello").withSearchCriteria("xxx").build();
 	}
 
 	@Test
 	public void checkConditionPositive() {
-		FilterCondition condition = FilterCondition.builder().withValue("hello").withSearchCriteria("xxx").withCondition(Condition.EQUALS)
+		FilterCondition condition = FilterCondition.builder()
+				.withValue("hello")
+				.withSearchCriteria("xxx")
+				.withCondition(Condition.EQUALS)
 				.build();
 		Assert.assertThat("Incorrect condition", condition.getCondition(), is(Condition.EQUALS));
 		Assert.assertThat("Incorrect value", condition.getValue(), is("hello"));
 		Assert.assertThat("Incorrect search criteria", condition.getSearchCriteria(), is("xxx"));
 	}
-
 
 	@Test
 	public void checkEquals() {
@@ -73,16 +71,19 @@ public class ConditionBuilderTest {
 
 		FilterCondition condition = FilterCondition.builder()
 				.withSearchCriteria("start_time")
-				.withValue( "0;1439;-6")
+				.withValue("0;1439;-6")
 				.withCondition(Condition.BETWEEN)
 				.build();
 		Assert.assertThat("Incorrect condition", condition.getCondition(), is(Condition.BETWEEN));
 
 		Criteria criteria = new Criteria();
-		condition.getCondition().addCondition(criteria, condition,
-				CriteriaMapFactory.DEFAULT_INSTANCE_SUPPLIER.get().getCriteriaMap(TestItem.class).getCriteriaHolder("start_time"));
+		condition.getCondition().addCondition(
+				criteria,
+				condition,
+				CriteriaMapFactory.DEFAULT_INSTANCE_SUPPLIER.get().getCriteriaMap(TestItem.class).getCriteriaHolder("start_time")
+		);
 
-		long expected = LocalDate.now().atStartOfDay(ZoneId.of("UTC")).plusHours(6).toEpochSecond();
+		long expected = ZonedDateTime.now(ZoneId.of("UTC")).toLocalDate().atStartOfDay(ZoneId.of("UTC")).plusHours(6).toEpochSecond();
 		long calculated = ((Date) (criteria.getCriteriaObject().get("$gte"))).toInstant().getEpochSecond();
 		Assert.assertThat("Incorrect between range processing", calculated, is(expected));
 
