0a6e5fd1334bcd5bf18dd271417b50001935b2ad
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-03-27 16:26:51 +0300

    introduce load/unload issue activities

--- src/main/java/com/epam/ta/reportportal/database/entity/item/ActivityEventType.java
+++ src/main/java/com/epam/ta/reportportal/database/entity/item/ActivityEventType.java
@@ -28,46 +28,46 @@ import java.util.Optional;
  */
 public enum ActivityEventType {
 
-    CREATE_DASHBOARD("create_dashboard"),
-    UPDATE_DASHBOARD("update_dashboard"),
-    DELETE_DASHBOARD("delete_dashboard"),
-    CREATE_WIDGET("create_widget"),
-    UPDATE_WIDGET("update_widget"),
-    DELETE_WIDGET("delete_widget"),
-    CREATE_FILTER("create_filter"),
-    UPDATE_FILTER("update_filter"),
-    DELETE_FILTER("delete_filter"),
-    ANALYZE_ITEM("analyze_item"),
-    UPDATE_DEFECT("update_defect"),
-    DELETE_DEFECT("delete_defect"),
-    CREATE_BTS("create_bts"),
-    UPDATE_BTS("update_bts"),
-    DELETE_BTS("delete_bts"),
-    START_LAUNCH("start_launch"),
-    FINISH_LAUNCH("finish_launch"),
-    DELETE_LAUNCH("delete_launch"),
-    UPDATE_PROJECT("update_project"),
-    POST_ISSUE("post_issue"),
-    ATTACH_ISSUE("attach_issue"),
-    ATTACH_ISSUE_AA("attach_issue_aa"),
-    UPDATE_ITEM("update_item"),
-    CREATE_USER("create_user"),
-    START_IMPORT("start_import"),
-    FINISH_IMPORT("finish_import");
+	CREATE_DASHBOARD("create_dashboard"),
+	UPDATE_DASHBOARD("update_dashboard"),
+	DELETE_DASHBOARD("delete_dashboard"),
+	CREATE_WIDGET("create_widget"),
+	UPDATE_WIDGET("update_widget"),
+	DELETE_WIDGET("delete_widget"),
+	CREATE_FILTER("create_filter"),
+	UPDATE_FILTER("update_filter"),
+	DELETE_FILTER("delete_filter"),
+	ANALYZE_ITEM("analyze_item"),
+	UPDATE_DEFECT("update_defect"),
+	DELETE_DEFECT("delete_defect"),
+	CREATE_BTS("create_bts"),
+	UPDATE_BTS("update_bts"),
+	DELETE_BTS("delete_bts"),
+	START_LAUNCH("start_launch"),
+	FINISH_LAUNCH("finish_launch"),
+	DELETE_LAUNCH("delete_launch"),
+	UPDATE_PROJECT("update_project"),
+	POST_ISSUE("post_issue"),
+	LOAD_ISSUE("load_issue"),
+	UNLOAD_ISSUE("unload_issue"),
+	ATTACH_ISSUE("attach_issue"),
+	ATTACH_ISSUE_AA("attach_issue_aa"),
+	UPDATE_ITEM("update_item"),
+	CREATE_USER("create_user"),
+	START_IMPORT("start_import"),
+	FINISH_IMPORT("finish_import");
 
-    private String value;
+	private String value;
 
-    ActivityEventType(String value) {
-        this.value = value;
-    }
+	ActivityEventType(String value) {
+		this.value = value;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public static Optional<ActivityEventType> fromString(String string) {
-        return Optional.ofNullable(string)
-                .flatMap(str -> Arrays.stream(values())
-                .filter(it -> it.value.equals(str)).findAny());
-    }
+	public static Optional<ActivityEventType> fromString(String string) {
+		return Optional.ofNullable(string).flatMap(str -> Arrays.stream(values()).filter(it -> it.value.equals(str)).findAny());
+	}
 }
