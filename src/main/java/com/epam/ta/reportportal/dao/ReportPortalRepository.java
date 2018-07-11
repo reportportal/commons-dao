c051c77b09c2485e85f6991ba4364f7ef3854926
Ivan_Budayeu <budaevqwerty@gmail.com>
2018-07-11 18:57:04 +0300

    existsByFilter() method renamed to exists()

--- src/main/java/com/epam/ta/reportportal/dao/ReportPortalRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/ReportPortalRepository.java
@@ -52,6 +52,6 @@ public interface ReportPortalRepository<T, ID extends Serializable> extends JpaR
 
 	<R> Page<R> findByFilter(Filter filter, Pageable pageable, RecordMapper<? super Record, R> mapper);
 
-	boolean existsWithMFilter(Filter filter);
+	boolean exists(Filter filter);
 
 }
