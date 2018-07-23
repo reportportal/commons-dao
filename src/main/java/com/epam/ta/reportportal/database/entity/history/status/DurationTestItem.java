72be3d44d71f564838f4efd5822dc6fa25528d58
pbortnik <pavel_bortnik@epam.com>
2018-07-23 19:14:15 +0300

    update duration result object

--- src/main/java/com/epam/ta/reportportal/database/entity/history/status/DurationTestItem.java
+++ src/main/java/com/epam/ta/reportportal/database/entity/history/status/DurationTestItem.java
@@ -1,21 +1,86 @@
+/*
+ * Copyright 2017 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/commons-dao
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.epam.ta.reportportal.database.entity.history.status;
 
 import com.epam.ta.reportportal.database.entity.Status;
 import com.epam.ta.reportportal.database.entity.item.TestItemType;
 
+import java.io.Serializable;
+
 /**
+ * Does not db object. Represents results of query
+ * {@link com.epam.ta.reportportal.database.dao.TestItemRepositoryCustom#findMostTimeConsumingTestItems(String, int)}}
+ * aggregation query.
+ *
  * @author Pavel Bortnik
  */
-public interface DurationTestItem {
-	String getId();
+public class DurationTestItem implements Serializable {
 
-	String getName();
+	private String id;
 
-	String getUniqueId();
+	private String name;
 
-	Long getDuration();
+	private String uniqueId;
 
-	Status getStatus();
+	private Status status;
 
-	TestItemType getType();
+	private TestItemType type;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUniqueId() {
+		return uniqueId;
+	}
+
+	public void setUniqueId(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public TestItemType getType() {
+		return type;
+	}
+
+	public void setType(TestItemType type) {
+		this.type = type;
+	}
 }
