cc6416ff0a4dc7d558ea50461ab6bda7a910624e
Ivan_Budayeu <budaevqwerty@gmail.com>
2018-07-26 12:40:58 +0300

    AuthConfig entity with repositories was replaced by Integration

--- src/main/java/com/epam/ta/reportportal/dao/IntegrationRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/IntegrationRepository.java
@@ -3,9 +3,7 @@ package com.epam.ta.reportportal.dao;
 import com.epam.ta.reportportal.entity.integration.Integration;
 
 /**
- * Repository for {@link com.epam.ta.reportportal.entity.integration.Integration} entity
- *
- * @author Yauheni_Martynau
+ * @author Ivan Budayeu
  */
 public interface IntegrationRepository extends ReportPortalRepository<Integration, Long>, IntegrationRepositoryCustom {
 }
--- src/main/java/com/epam/ta/reportportal/dao/IntegrationRepositoryCustom.java
+++ src/main/java/com/epam/ta/reportportal/dao/IntegrationRepositoryCustom.java
@@ -1,10 +1,24 @@
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.entity.integration.Integration;
+import com.epam.ta.reportportal.entity.ldap.ActiveDirectoryConfig;
+import com.epam.ta.reportportal.entity.ldap.LdapConfig;
+
+import java.util.Optional;
 
 /**
  * @author Yauheni_Martynau
  */
 public interface IntegrationRepositoryCustom extends FilterableRepository<Integration>{
+
+	void deleteSettings(Integration integration);
+
+	void updateLdap(LdapConfig ldapConfig);
+
+	void updateActiveDirectory(ActiveDirectoryConfig adConfig);
+
+	Optional<LdapConfig> findLdap(boolean enabled);
+
+	Optional<ActiveDirectoryConfig> findActiveDirectory(boolean enabled);
 
 }
