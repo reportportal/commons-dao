d40667108b73bd38a2bae789acd25f3edf657855
pbortnik <rrrizon@gmail.com>
2018-08-20 12:39:08 +0300

    upd jooq

--- src/main/java/com/epam/ta/reportportal/dao/OAuthRegistrationRestrictionRepositoryCustom.java
+++ src/main/java/com/epam/ta/reportportal/dao/OAuthRegistrationRestrictionRepositoryCustom.java
@@ -1,9 +1,5 @@
 package com.epam.ta.reportportal.dao;
 
-import com.epam.ta.reportportal.entity.oauth.OAuthRegistrationRestriction;
-
-import java.util.List;
-
 /**
  * @author Anton Machulski
  */
--- src/main/java/com/epam/ta/reportportal/dao/OAuthRegistrationRestrictionRepositoryCustomImpl.java
+++ src/main/java/com/epam/ta/reportportal/dao/OAuthRegistrationRestrictionRepositoryCustomImpl.java
@@ -1,13 +1,6 @@
 package com.epam.ta.reportportal.dao;
 
-import com.epam.ta.reportportal.entity.oauth.OAuthRegistrationRestriction;
-import org.jooq.DSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-import static com.epam.ta.reportportal.jooq.Tables.OAUTH_REGISTRATION_RESTRICTION;
 
 /**
  * @author Anton Machulski
