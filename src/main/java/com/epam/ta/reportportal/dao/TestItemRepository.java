c293b9454c78d8c8bc55cf29e1bbbedbd92e94d7
pbortnik <rrrizon@gmail.com>
2018-08-27 17:55:06 +0300

    rename params

--- src/main/java/com/epam/ta/reportportal/dao/TestItemRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/TestItemRepository.java
@@ -44,8 +44,8 @@ public interface TestItemRepository extends ReportPortalRepository<TestItem, Lon
 	 * @param itemPath Current item path in a tree
 	 * @return True if has
 	 */
-	@Query(value = "SELECT EXISTS(SELECT 1 FROM test_item WHERE test_item.path ~ (:path || '.*{1}')::LQUERY LIMIT 1)", nativeQuery = true)
-	boolean hasChildren(@Param(value = "path") String itemPath);
+	@Query(value = "SELECT EXISTS(SELECT 1 FROM test_item WHERE test_item.path ~ (:itemPath || '.*{1}')::LQUERY LIMIT 1)", nativeQuery = true)
+	boolean hasChildren(@Param(value = "itemPath") String itemPath);
 
 	/**
 	 * Select ids and names of all items in a tree till current.
@@ -53,6 +53,6 @@ public interface TestItemRepository extends ReportPortalRepository<TestItem, Lon
 	 * @param itemPath itemPath
 	 * @return Map of id -> name
 	 */
-	@Query(value = "SELECT item_id, name FROM test_item WHERE test_item.path @> :path::LTREE", nativeQuery = true)
-	Map<Long, String> selectPathNames(@Param(value = "path") String itemPath);
+	@Query(value = "SELECT item_id, name FROM test_item WHERE test_item.path @> :itemPath::LTREE", nativeQuery = true)
+	Map<Long, String> selectPathNames(@Param(value = "itemPath") String itemPath);
 }
