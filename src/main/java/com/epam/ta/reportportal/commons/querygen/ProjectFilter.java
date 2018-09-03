5f80b09bda7ed801d20a32bfd2b8eccf6e8133b7
yumfriez <budaevqwerty@gmail.com>
2018-09-03 11:28:47 +0300

    ProjectFilter entity refactoring

--- src/main/java/com/epam/ta/reportportal/commons/querygen/ProjectFilter.java
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/ProjectFilter.java
@@ -6,13 +6,26 @@ public class ProjectFilter extends Filter {
 
 	private ProjectFilter(Long id, Filter filter, Long projectId) {
 		super(id, filter.getTarget(), filter.getFilterConditions());
-		getFilterConditions().add(new FilterCondition(
-				Condition.EQUALS,
+		getFilterConditions().add(new FilterCondition(Condition.EQUALS,
 				false,
 				String.valueOf(projectId),
 				GeneralCriteriaConstant.PROJECT_ID
 		));
 
+	}
+
+	private ProjectFilter(Filter filter, Long projectId) {
+		super(filter.getTarget(), filter.getFilterConditions());
+		getFilterConditions().add(new FilterCondition(Condition.EQUALS,
+				false,
+				String.valueOf(projectId),
+				GeneralCriteriaConstant.PROJECT_ID
+		));
+
+	}
+
+	public static Filter of(Filter filter, Long projectId) {
+		return new ProjectFilter(filter, projectId);
 	}
 
 	public static Filter of(Long id, Filter filter, Long projectId) {
