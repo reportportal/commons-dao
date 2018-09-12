452f60bf26051e49ff5999fa2b2b8d20957cdb50
Andrey_Plisunov <andrey_plisunov@epam.com>
2018-09-12 14:23:14 +0300

    EPMRPP-35818 Re-implement project configuration into project attributes

--- src/main/java/com/epam/ta/reportportal/entity/project/ProjectConfiguration.java
+++ src/main/java/com/epam/ta/reportportal/entity/project/ProjectConfiguration.java
@@ -4,6 +4,7 @@ import com.epam.ta.reportportal.entity.enums.EntryType;
 import com.epam.ta.reportportal.entity.project.email.ProjectEmailConfig;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.Serializable;
 import java.util.*;
@@ -41,10 +42,10 @@ public class ProjectConfiguration implements Serializable {
 			this.interruptJobTime = attributes.get(INTERRUPT_JOB_TIME.getValue());
 			this.keepLogs = attributes.get(KEEP_LOGS.getValue());
 			this.keepScreenshots = attributes.get(KEEP_SCREENSHOTS.getValue());
-			this.analyzerConfig =  new ProjectAnalyzerConfig(getOptionValueOrDefault(attributes.get(MIN_DOC_FREQ.getValue()), ProjectAnalyzerConfig.MIN_DOC_FREQ),
-															 getOptionValueOrDefault(attributes.get(MIN_TERM_FREQ.getValue()), ProjectAnalyzerConfig.MIN_TERM_FREQ),
-															 getOptionValueOrDefault(attributes.get(MIN_SHOULD_MATCH.getValue()), ProjectAnalyzerConfig.MIN_SHOULD_MATCH),
-															 getOptionValueOrDefault(attributes.get(NUMBER_OF_LOG_LINES.getValue()), ProjectAnalyzerConfig.NUMBER_OF_LOG_LINES),
+			this.analyzerConfig =  new ProjectAnalyzerConfig(NumberUtils.toInt(attributes.get(MIN_DOC_FREQ.getValue()), ProjectAnalyzerConfig.MIN_DOC_FREQ),
+                                                             NumberUtils.toInt(attributes.get(MIN_TERM_FREQ.getValue()), ProjectAnalyzerConfig.MIN_TERM_FREQ),
+                                                             NumberUtils.toInt(attributes.get(MIN_SHOULD_MATCH.getValue()), ProjectAnalyzerConfig.MIN_SHOULD_MATCH),
+                                                             NumberUtils.toInt(attributes.get(NUMBER_OF_LOG_LINES.getValue()), ProjectAnalyzerConfig.NUMBER_OF_LOG_LINES),
 															 BooleanUtils.toBoolean(attributes.get(INDEXING_RUNNING.getValue())),
 															 BooleanUtils.toBooleanObject(attributes.get(AUTO_ANALYZER_ENABLED.getValue())));
 			this.projectEmailConfig = new ProjectEmailConfig(BooleanUtils.toBooleanObject(attributes.get(EMAIL_ENABLED.getValue())), attributes.get(EMAIL_FROM.getValue()));
@@ -98,13 +99,5 @@ public class ProjectConfiguration implements Serializable {
 
 	public void setProjectEmailConfig(ProjectEmailConfig projectEmailConfig) {
 		this.projectEmailConfig = projectEmailConfig;
-	}
-
-	private int getOptionValueOrDefault(String optionValue, int defaultValue) {
-		try {
-			return Integer.parseInt(optionValue);
-		} catch (NumberFormatException e) {
-			return defaultValue;
-		}
 	}
 }
