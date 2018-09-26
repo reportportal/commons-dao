0851c69c6fac1c9b0a687fe3c4985b17acdea497
pbortnik <rrrizon@gmail.com>
2018-09-26 21:00:08 +0300

    introduce user filter fetcher

--- src/main/java/com/epam/ta/reportportal/dao/UserFilterRepositoryCustomImpl.java
+++ src/main/java/com/epam/ta/reportportal/dao/UserFilterRepositoryCustomImpl.java
@@ -1,19 +1,54 @@
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.commons.querygen.Filter;
+import com.epam.ta.reportportal.commons.querygen.FilterCondition;
 import com.epam.ta.reportportal.commons.querygen.QueryBuilder;
+import com.epam.ta.reportportal.entity.filter.FilterSort;
 import com.epam.ta.reportportal.entity.filter.UserFilter;
+import com.epam.ta.reportportal.entity.project.Project;
+import com.google.common.collect.Lists;
 import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static com.epam.ta.reportportal.dao.constant.WidgetContentRepositoryConstants.*;
 
 @Repository
 public class UserFilterRepositoryCustomImpl implements UserFilterRepositoryCustom {
+
+	private static final Function<Result<? extends Record>, List<UserFilter>> USER_FILTER_FETCHER = result -> {
+		Map<Long, UserFilter> userFilterMap = new HashMap<>();
+		result.forEach(r -> {
+			Long userFilterID = r.get("id", Long.class);
+			UserFilter userFilter = null;
+			if (userFilterMap.containsKey(userFilterID)) {
+				userFilter = userFilterMap.get(userFilterID);
+			} else {
+				userFilter = new UserFilter();
+				userFilter.setId(userFilterID);
+				userFilter.setName(r.get(NAME, String.class));
+				userFilter.setDescription(r.get(DESCRIPTION, String.class));
+				userFilter.setTargetClass(r.get(TARGET, Class.class));
+				Project project = new Project();
+				project.setId(r.get(PROJECT_ID, Long.class));
+				userFilter.setProject(project);
+				userFilterMap.put(userFilterID, userFilter);
+			}
+			userFilter.getFilterCondition().add(r.into(FilterCondition.class));
+			userFilter.getFilterSorts().add(r.into(FilterSort.class));
+		});
+		return Lists.newArrayList(userFilterMap.values());
+	};
 
 	private final DSLContext dsl;
 
@@ -24,13 +59,13 @@ public class UserFilterRepositoryCustomImpl implements UserFilterRepositoryCusto
 
 	@Override
 	public List<UserFilter> findByFilter(Filter filter) {
-		return dsl.fetch(QueryBuilder.newBuilder(filter).build()).into(UserFilter.class);
+		return USER_FILTER_FETCHER.apply(dsl.fetch(QueryBuilder.newBuilder(filter).build()));
 	}
 
 	@Override
 	public Page<UserFilter> findByFilter(Filter filter, Pageable pageable) {
 		return PageableExecutionUtils.getPage(
-				dsl.fetch(QueryBuilder.newBuilder(filter).with(pageable).build()).into(UserFilter.class),
+				USER_FILTER_FETCHER.apply(dsl.fetch(QueryBuilder.newBuilder(filter).with(pageable).build())),
 				pageable,
 				() -> dsl.fetchCount(QueryBuilder.newBuilder(filter).build())
 		);
--- src/main/java/com/epam/ta/reportportal/dao/constant/WidgetContentRepositoryConstants.java
+++ src/main/java/com/epam/ta/reportportal/dao/constant/WidgetContentRepositoryConstants.java
@@ -28,6 +28,8 @@ public class WidgetContentRepositoryConstants {
 	public static final Integer ZERO_QUERY_VALUE = 0;
 	public static final String LAUNCH_ID = "launch_id";
 	public static final String NAME = "name";
+	public static final String DESCRIPTION = "description";
+	public static final String TARGET = "target";
 	public static final String ID = "id";
 	public static final String NUMBER = "number";
 	public static final String START_TIME = "start_time";
