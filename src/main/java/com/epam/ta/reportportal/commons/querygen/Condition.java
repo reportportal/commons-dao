04c29648bcf7f204c99b25d44cbdc14d2ee04c06
pbortnik <rrrizon@gmail.com>
2018-10-04 15:57:11 +0300

    move path validation into filter rules

--- src/main/java/com/epam/ta/reportportal/commons/querygen/Condition.java
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/Condition.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) 2018 EPAM Systems
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.epam.ta.reportportal.commons.querygen;
 
 import com.epam.ta.reportportal.ws.model.ErrorType;
@@ -116,9 +132,10 @@ public enum Condition {
 
 		@Override
 		public void validate(CriteriaHolder criteriaHolder, String value, boolean isNegative, ErrorType errorType) {
-			expect(criteriaHolder, it -> it.getFilterCriteria().equalsIgnoreCase("path")).verify(errorType,
-					"Applied only to 'path' parameter"
-			);
+			expect(criteriaHolder, or(filterForNumbers(), filterForLtree())).verify(errorType, formattedSupplier(
+					"'Level' condition is applyable only for positive Numbers and 'path' filter condition. Type of field is '{}'",
+					criteriaHolder.getDataType().getSimpleName()
+			));
 		}
 
 		@Override
