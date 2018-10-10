101ce8d3e0060f3f18d94bf97f9bda292ad1b179
yumfriez <budaevqwerty@gmail.com>
2018-10-10 14:10:53 +0300

    ServerSettings repository removing by term method impl

--- src/main/java/com/epam/ta/reportportal/dao/ServerSettingsRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/ServerSettingsRepository.java
@@ -1,9 +1,18 @@
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.entity.ServerSettings;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * @author Ivan Budaev
  */
 public interface ServerSettingsRepository extends ReportPortalRepository<ServerSettings, Long> {
+
+	@Query("from ServerSettings ss")
+	Stream<ServerSettings> streamAll();
+
+	Optional<ServerSettings> findByKey(String key);
 }
