d2e30d23fea17a4dcb8813ab9b830853b28e54aa
yumfriez <budaevqwerty@gmail.com>
2018-10-10 11:51:37 +0300

    user repository refactoring

--- src/main/java/com/epam/ta/reportportal/dao/UserRepositoryCustomImpl.java
+++ src/main/java/com/epam/ta/reportportal/dao/UserRepositoryCustomImpl.java
@@ -6,16 +6,8 @@ import com.epam.ta.reportportal.commons.querygen.QueryBuilder;
 import com.epam.ta.reportportal.commons.querygen.Queryable;
 import com.epam.ta.reportportal.dao.util.JooqFieldNameTransformer;
 import com.epam.ta.reportportal.entity.user.User;
-import com.epam.ta.reportportal.exception.ReportPortalException;
 import com.epam.ta.reportportal.filesystem.DataStore;
 import com.epam.ta.reportportal.jooq.tables.JUsers;
-import com.epam.ta.reportportal.ws.model.ErrorType;
-import org.apache.commons.io.IOUtils;
-import org.apache.tika.io.TikaInputStream;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
-import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.parser.image.ImageParser;
 import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.jooq.Record;
@@ -27,9 +19,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -68,20 +57,6 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 	@Override
 	public String replaceUserPhoto(User user, BinaryData binaryData) {
 		return dataStore.save(user.getLogin(), binaryData.getInputStream());
-	}
-
-	@Override
-	public BinaryData findUserPhoto(String path) {
-		InputStream inputStream = dataStore.load(path);
-		try {
-			byte[] bytes = IOUtils.toByteArray(inputStream);
-			String contentType = resolveContentType(bytes);
-
-			return new BinaryData(contentType, (long) bytes.length, new ByteArrayInputStream(bytes));
-		} catch (IOException e) {
-			//TODO add new exception type
-			throw new ReportPortalException(ErrorType.BAD_REQUEST_ERROR);
-		}
 	}
 
 	@Override
@@ -129,12 +104,4 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 		);
 	}
 
-	private String resolveContentType(byte[] data) {
-		AutoDetectParser parser = new AutoDetectParser(new ImageParser());
-		try {
-			return parser.getDetector().detect(TikaInputStream.get(data), new Metadata()).toString();
-		} catch (IOException e) {
-			return MediaType.OCTET_STREAM.toString();
-		}
-	}
 }
