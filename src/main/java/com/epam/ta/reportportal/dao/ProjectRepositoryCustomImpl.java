d197ff6f3207753a6868b61a4d015ef5cf5c67a4
pbortnik <rrrizon@gmail.com>
2018-10-11 22:08:27 +0300

    fix aliased table

--- src/main/java/com/epam/ta/reportportal/dao/ProjectRepositoryCustomImpl.java
+++ src/main/java/com/epam/ta/reportportal/dao/ProjectRepositoryCustomImpl.java
@@ -17,9 +17,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+import static com.epam.ta.reportportal.dao.util.JooqFieldNameTransformer.fieldName;
 import static com.epam.ta.reportportal.dao.util.RecordMappers.PROJECT_MAPPER;
 import static com.epam.ta.reportportal.jooq.Tables.*;
-import static org.jooq.impl.DSL.*;
+import static org.jooq.impl.DSL.choose;
+import static org.jooq.impl.DSL.name;
 
 @Repository
 public class ProjectRepositoryCustomImpl implements ProjectRepositoryCustom {
@@ -44,23 +46,23 @@ public class ProjectRepositoryCustomImpl implements ProjectRepositoryCustom {
 
 	@Override
 	public Page<ProjectInfo> findProjectInfoByFilter(Filter filter, Pageable pageable, String mode) {
-		return PageableExecutionUtils.getPage(dsl.with("filtered_project")
+		final String FILTERED_PROJECT = "filtered_project";
+		return PageableExecutionUtils.getPage(dsl.with(FILTERED_PROJECT)
 				.as(QueryBuilder.newBuilder(filter).with(pageable).build())
-				.select(
-						DSL.countDistinct(PROJECT_USER.USER_ID).as("usersQuantity"),
+				.select(DSL.countDistinct(PROJECT_USER.USER_ID).as("usersQuantity"),
 						DSL.countDistinct(choose().when(LAUNCH.MODE.eq(JLaunchModeEnum.valueOf(mode))
 								.and(LAUNCH.STATUS.ne(JStatusEnum.IN_PROGRESS)), LAUNCH.ID)).as("launchesQuantity"),
 						DSL.max(LAUNCH.START_TIME).as("lastRun"),
-						PROJECT.ID,
-						PROJECT.CREATION_DATE,
-						PROJECT.NAME,
-						PROJECT.PROJECT_TYPE
+						fieldName(FILTERED_PROJECT, PROJECT.ID.getName()),
+						fieldName(FILTERED_PROJECT, PROJECT.CREATION_DATE.getName()),
+						fieldName(FILTERED_PROJECT, PROJECT.NAME.getName()),
+						fieldName(FILTERED_PROJECT, PROJECT.PROJECT_TYPE.getName())
 				)
-				.from(DSL.table(name("filtered_project"))
+				.from(DSL.table(name(FILTERED_PROJECT))
 						.leftJoin(PROJECT_USER)
-						.on(field(name("filtered_project", PROJECT.ID.getName())).cast(Long.class).eq(PROJECT_USER.PROJECT_ID))
+						.on(fieldName(FILTERED_PROJECT, PROJECT.ID.getName()).cast(Long.class).eq(PROJECT_USER.PROJECT_ID))
 						.leftJoin(LAUNCH)
-						.on(field(name("filtered_project", PROJECT.ID.getName())).cast(Long.class).eq(LAUNCH.PROJECT_ID)))
+						.on(fieldName(FILTERED_PROJECT, PROJECT.ID.getName()).cast(Long.class).eq(LAUNCH.PROJECT_ID)))
 				.groupBy(PROJECT.ID, PROJECT.CREATION_DATE, PROJECT.NAME, PROJECT.PROJECT_TYPE)
 				.fetch()
 				.into(ProjectInfo.class), pageable, () -> dsl.fetchCount(QueryBuilder.newBuilder(filter).build()));
