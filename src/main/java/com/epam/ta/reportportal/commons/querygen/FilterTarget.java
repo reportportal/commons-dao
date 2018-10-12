a91f009fd4d8eccccc4f7473b38403cf3efc462b
pbortnik <rrrizon@gmail.com>
2018-10-12 13:18:48 +0300

    add log level to filter target

--- src/main/java/com/epam/ta/reportportal/commons/querygen/FilterTarget.java
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/FilterTarget.java
@@ -48,8 +48,7 @@ import static com.epam.ta.reportportal.commons.querygen.constant.ActivityCriteri
 import static com.epam.ta.reportportal.commons.querygen.constant.IntegrationCriteriaConstant.TYPE;
 import static com.epam.ta.reportportal.commons.querygen.constant.LaunchCriteriaConstant.MODE;
 import static com.epam.ta.reportportal.commons.querygen.constant.LaunchCriteriaConstant.STATUS;
-import static com.epam.ta.reportportal.commons.querygen.constant.LogCriteriaConstant.CRITERIA_LOG_MESSAGE;
-import static com.epam.ta.reportportal.commons.querygen.constant.LogCriteriaConstant.CRITERIA_TEST_ITEM_ID;
+import static com.epam.ta.reportportal.commons.querygen.constant.LogCriteriaConstant.*;
 import static com.epam.ta.reportportal.commons.querygen.constant.TestItemCriteriaConstant.*;
 import static com.epam.ta.reportportal.commons.querygen.constant.UserCriteriaConstant.*;
 import static com.epam.ta.reportportal.dao.constant.WidgetContentRepositoryConstants.*;
@@ -270,7 +269,8 @@ public enum FilterTarget {
 			},
 
 	LOG(Log.class, Arrays.asList(new CriteriaHolder(CRITERIA_LOG_MESSAGE, "l.log_message", String.class, false),
-			new CriteriaHolder(CRITERIA_TEST_ITEM_ID, "l.item_id", Long.class, false)
+			new CriteriaHolder(CRITERIA_TEST_ITEM_ID, "l.item_id", Long.class, false),
+			new CriteriaHolder(CRITERIA_LOG_LEVEL, "l.level", Integer.class, false)
 	))
 
 			{
