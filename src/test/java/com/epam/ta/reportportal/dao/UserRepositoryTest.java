8d8dc06d7a7ebb0c5b5fa06d67a3002d375b86c3
yumfriez <budaevqwerty@gmail.com>
2018-10-12 12:43:00 +0300

    User metadata was replaced by JsonMap

--- src/test/java/com/epam/ta/reportportal/dao/UserRepositoryTest.java
+++ src/test/java/com/epam/ta/reportportal/dao/UserRepositoryTest.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright (C) 2018 EPAM Systems
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.commons.querygen.Condition;
@@ -6,7 +24,7 @@ import com.epam.ta.reportportal.commons.querygen.FilterCondition;
 import com.epam.ta.reportportal.config.TestConfiguration;
 import com.epam.ta.reportportal.config.util.SqlRunner;
 import com.epam.ta.reportportal.entity.Activity;
-import com.epam.ta.reportportal.entity.JsonbObject;
+import com.epam.ta.reportportal.entity.JsonMap;
 import com.epam.ta.reportportal.entity.meta.MetaData;
 import com.epam.ta.reportportal.entity.project.Project;
 import com.epam.ta.reportportal.entity.project.ProjectRole;
@@ -26,6 +44,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -57,15 +76,15 @@ public class UserRepositoryTest {
 
 	@BeforeClass
 	public static void init() throws SQLException, ClassNotFoundException, IOException, SqlToolError {
-		Class.forName("org.hsqldb.jdbc.JDBCDriver");
-		runSqlScript("/test-dropall-script.sql");
-		runSqlScript("/test-create-script.sql");
-		runSqlScript("/test-fill-script.sql");
+		//		Class.forName("org.hsqldb.jdbc.JDBCDriver");
+		//		runSqlScript("/test-dropall-script.sql");
+		//		runSqlScript("/test-create-script.sql");
+		//		runSqlScript("/test-fill-script.sql");
 	}
 
 	@AfterClass
 	public static void destroy() throws SQLException, IOException, SqlToolError {
-		runSqlScript("/test-dropall-script.sql");
+		//		runSqlScript("/test-dropall-script.sql");
 	}
 
 	private static void runSqlScript(String scriptPath) throws SQLException, IOException, SqlToolError {
@@ -98,7 +117,7 @@ public class UserRepositoryTest {
 	@Test
 	public void findByLogin() {
 
-		Optional<User> user = userRepository.findByLogin("superadmin");
+		Optional<User> user = userRepository.findByLogin("default");
 
 		Assert.assertTrue(user.isPresent());
 	}
@@ -169,11 +188,12 @@ public class UserRepositoryTest {
 	}
 
 	@Test
+	@Rollback(false)
 	public void saveUserTest() throws JsonProcessingException {
 		User user = userRepository.findByLogin("default").get();
 		Map<String, Object> hashMap = new HashMap<>();
 		hashMap.put("asd", "qwe");
-		JsonbObject metaData = new MetaData(hashMap);
+		JsonMap<Object, Object> metaData = new JsonMap<>(hashMap);
 		user.setMetadata(metaData);
 
 		userRepository.save(user);
@@ -237,7 +257,7 @@ public class UserRepositoryTest {
 
 		map.put("last_login", new Date());
 
-		reg.setMetadata(new MetaData(map));
+		reg.setMetadata(new JsonMap<>(map));
 
 		Set<ProjectUser> projectUsers = defaultProject.getUsers();
 		//noinspection ConstantConditions
