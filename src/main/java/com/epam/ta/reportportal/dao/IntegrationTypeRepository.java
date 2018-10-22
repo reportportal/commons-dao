e0bc2b5f4282cfe1ff5b18d28d7aa75bc13ddb18
avarabyeu <andrei.varabyeu@gmail.com>
2018-10-22 14:20:50 +0300

    add several methods to integration type repository
    Signed-off-by: avarabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/dao/IntegrationTypeRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/IntegrationTypeRepository.java
@@ -2,10 +2,29 @@ package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.entity.integration.IntegrationType;
 
+import java.util.Optional;
+
 /**
  * Repository for {@link com.epam.ta.reportportal.entity.integration.IntegrationType} entity
  *
  * @author Yauheni_Martynau
  */
 public interface IntegrationTypeRepository extends ReportPortalRepository<IntegrationType, Long> {
+
+	/**
+	 * Searches for an integration
+	 *
+	 * @param name Integration name
+	 * @return Optional of integration
+	 */
+	Optional<IntegrationType> findByName(String name);
+
+	/**
+	 * Searches for an integration by name and group
+	 *
+	 * @param name      Integration name
+	 * @param groupType Integration group
+	 * @return Optional of integration
+	 */
+	Optional<IntegrationType> findByNameAndGroupType(String name, String groupType);
 }
