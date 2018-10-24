31861b97a6eb629666803844f2624931a7457922
yumfriez <budaevqwerty@gmail.com>
2018-10-24 14:55:19 +0300

    Integration params fix

--- src/main/java/com/epam/ta/reportportal/entity/integration/Integration.java
+++ src/main/java/com/epam/ta/reportportal/entity/integration/Integration.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) 2018 EPAM Systems
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.epam.ta.reportportal.entity.integration;
 
 import com.epam.ta.reportportal.commons.JsonbUserType;
@@ -17,7 +33,7 @@ import java.time.LocalDateTime;
  */
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@TypeDef(name = "jsonb", typeClass = JsonbUserType.class)
+@TypeDef(name = "integration_params", typeClass = JsonbUserType.class)
 @Table(name = "integration", schema = "public")
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Integration implements Serializable {
@@ -36,7 +52,7 @@ public class Integration implements Serializable {
 	@JoinColumn(name = "type")
 	private IntegrationType type;
 
-	@Type(type = "jsonb")
+	@Type(type = "integration_params")
 	@Column(name = "params")
 	private IntegrationParams params;
 
