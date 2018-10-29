6c2e81681aeb5ad1770c0858de6af1d8365d80ac
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-10-29 17:53:34 +0300

    Add native query

--- src/main/java/com/epam/ta/reportportal/dao/StatisticsFieldRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/StatisticsFieldRepository.java
@@ -1,11 +1,14 @@
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.entity.statistics.StatisticsField;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
 public interface StatisticsFieldRepository extends ReportPortalRepository<StatisticsField, Long> {
 
-	void deleteByName(String name);
+	@Query("DELETE FROM statistics_field WHERE name=:name")
+	void deleteByName(@Param("name") String name);
 }
