9ffb5bf8b5b0e29c5e3938820887d8af70beddb1
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-10-29 18:36:29 +0300

    Change to spring data jpa

--- src/main/java/com/epam/ta/reportportal/dao/StatisticsFieldRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/StatisticsFieldRepository.java
@@ -1,14 +1,11 @@
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.entity.statistics.StatisticsField;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
 public interface StatisticsFieldRepository extends ReportPortalRepository<StatisticsField, Long> {
 
-	@Query(value = "DELETE FROM statistics_field WHERE name=:name", nativeQuery = true)
-	void deleteByName(@Param("name") String name);
+	void deleteByName(String name);
 }
