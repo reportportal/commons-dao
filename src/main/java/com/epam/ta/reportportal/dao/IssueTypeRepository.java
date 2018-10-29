81d9711c9fd1c042fed5fa2fe68f382902e11a49
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-10-29 20:47:48 +0300

    Add issue and issue type repositories

---
+++ src/main/java/com/epam/ta/reportportal/dao/IssueTypeRepository.java
@@ -0,0 +1,7 @@
+package com.epam.ta.reportportal.dao;
+
+import com.epam.ta.reportportal.entity.item.issue.IssueType;
+
+public interface IssueTypeRepository extends ReportPortalRepository<IssueType, Long> {
+
+}
