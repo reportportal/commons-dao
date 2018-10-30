c5cfdd074b1012d74d5b4cf53e8641ee08f983fa
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-10-30 10:50:04 +0300

    Add findByLocator method

--- src/main/java/com/epam/ta/reportportal/dao/IssueTypeRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/IssueTypeRepository.java
@@ -4,4 +4,5 @@ import com.epam.ta.reportportal.entity.item.issue.IssueType;
 
 public interface IssueTypeRepository extends ReportPortalRepository<IssueType, Long> {
 
+	IssueType findByLocator(String locator);
 }
