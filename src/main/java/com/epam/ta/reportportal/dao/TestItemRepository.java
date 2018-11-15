1ed9960f93b6b40371460e9a3895544b0de53ed9
yumfriez <budaevqwerty@gmail.com>
2018-11-15 15:50:14 +0300

    Handle retries function impl

--- src/main/java/com/epam/ta/reportportal/dao/TestItemRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/TestItemRepository.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright (C) 2018 EPAM Systems
+ * Copyright (C) 2018 EPAM Systems
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.epam.ta.reportportal.dao;
@@ -31,6 +31,9 @@ public interface TestItemRepository extends ReportPortalRepository<TestItem, Lon
 	List<TestItem> findTestItemsByUniqueId(String uniqueId);
 
 	List<TestItem> findTestItemsByLaunchId(Long launchId);
+
+	@Query(value = "SELECT handle_retries(:itemId)", nativeQuery = true)
+	void handleRetries(@Param("itemId") Long itemId);
 
 	@Query(value = "DELETE FROM test_item WHERE test_item.item_id = :itemId", nativeQuery = true)
 	void deleteTestItem(@Param(value = "itemId") Long itemId);
