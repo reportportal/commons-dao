1976cc0e45a7bb7e50ebd27b999786ecbca2d423
pavel_bortnik <pavel_bortnik@epam.com>
2018-11-19 14:41:08 +0300

    fix log filter target

--- src/main/java/com/epam/ta/reportportal/commons/querygen/FilterTarget.java
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/FilterTarget.java
@@ -24,6 +24,7 @@ import com.epam.ta.reportportal.entity.filter.UserFilter;
 import com.epam.ta.reportportal.entity.integration.Integration;
 import com.epam.ta.reportportal.entity.item.TestItem;
 import com.epam.ta.reportportal.entity.launch.Launch;
+import com.epam.ta.reportportal.entity.log.Log;
 import com.epam.ta.reportportal.entity.project.Project;
 import com.epam.ta.reportportal.entity.user.User;
 import com.epam.ta.reportportal.jooq.enums.JLaunchModeEnum;
