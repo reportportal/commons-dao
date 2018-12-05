72b998f69274b72a95f1b5dde35425ea0a115d2c
yumfriez <budaevqwerty@gmail.com>
2018-12-05 20:10:59 +0300

    UserFilter, Widget and Dashboard DAOs structure has been updated

--- src/main/java/com/epam/ta/reportportal/dao/WidgetRepositoryCustomImpl.java
+++ src/main/java/com/epam/ta/reportportal/dao/WidgetRepositoryCustomImpl.java
@@ -17,60 +17,31 @@
 package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.commons.querygen.ProjectFilter;
-import com.epam.ta.reportportal.commons.querygen.QueryBuilder;
 import com.epam.ta.reportportal.entity.widget.Widget;
-import org.jooq.DSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import static com.epam.ta.reportportal.dao.util.ResultFetchers.WIDGET_FETCHER;
-import static com.epam.ta.reportportal.dao.util.ShareableUtils.*;
 
 /**
  * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
  */
 @Repository
-public class WidgetRepositoryCustomImpl implements WidgetRepositoryCustom {
-
-	@Autowired
-	private DSLContext dsl;
+public class WidgetRepositoryCustomImpl extends AbstractShareableRepositoryImpl<Widget> implements WidgetRepositoryCustom {
 
 	@Override
 	public Page<Widget> getPermitted(ProjectFilter filter, Pageable pageable, String userName) {
-		return PageableExecutionUtils.getPage(
-				WIDGET_FETCHER.apply(dsl.fetch(QueryBuilder.newBuilder(filter)
-						.addCondition(permittedCondition(userName))
-						.withWrapper(filter.getTarget())
-						.build())),
-				pageable,
-				() -> dsl.fetchCount(QueryBuilder.newBuilder(filter).addCondition(permittedCondition(userName)).build())
-		);
+		return getPermitted(WIDGET_FETCHER, filter, pageable, userName);
 	}
 
 	@Override
 	public Page<Widget> getOwn(ProjectFilter filter, Pageable pageable, String userName) {
-		return PageableExecutionUtils.getPage(
-				WIDGET_FETCHER.apply(dsl.fetch(QueryBuilder.newBuilder(filter)
-						.addCondition(ownCondition(userName))
-						.withWrapper(filter.getTarget())
-						.build())),
-				pageable,
-				() -> dsl.fetchCount(QueryBuilder.newBuilder(filter).addCondition(ownCondition(userName)).build())
-		);
+		return getOwn(WIDGET_FETCHER, filter, pageable, userName);
 	}
 
 	@Override
 	public Page<Widget> getShared(ProjectFilter filter, Pageable pageable, String userName) {
-		return PageableExecutionUtils.getPage(
-				WIDGET_FETCHER.apply(dsl.fetch(QueryBuilder.newBuilder(filter)
-						.addCondition(sharedCondition(userName))
-						.withWrapper(filter.getTarget())
-						.build())),
-				pageable,
-				() -> dsl.fetchCount(QueryBuilder.newBuilder(filter).addCondition(sharedCondition(userName)).build())
-		);
+		return getShared(WIDGET_FETCHER, filter, pageable, userName);
 	}
 }
