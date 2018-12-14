f46d5dc438319fd907e6a6f3e3b8a6146cf4a95d
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-12-14 14:15:14 +0300

    Merge branch 'postgres-migration' of github.com:reportportal/commons-dao into tests-opentable
    # Conflicts:
    #	src/main/java/com/epam/ta/reportportal/dao/ActivityRepositoryCustomImpl.java
    #	src/test/java/com/epam/ta/reportportal/dao/LaunchRepositoryTest.java

--- src/main/java/com/epam/ta/reportportal/dao/ActivityRepositoryCustomImpl.java
+++ src/main/java/com/epam/ta/reportportal/dao/ActivityRepositoryCustomImpl.java
@@ -56,7 +56,7 @@ public class ActivityRepositoryCustomImpl implements ActivityRepositoryCustom {
 	}
 
 	@Override
-	public List<Activity> findByFilterWithSortingAndLimit(Filter filter, Sort sort, int limit) {
+	public List<Activity> findByFilterWithSortingAndLimit(Queryable filter, Sort sort, int limit) {
 		return dsl.fetch(QueryBuilder.newBuilder(filter).with(sort).with(limit).withWrapper(filter.getTarget()).build())
 				.map(ACTIVITY_MAPPER);
 	}
