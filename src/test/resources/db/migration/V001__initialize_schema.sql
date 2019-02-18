3d8886efa5adb2a410c5d445f884fbf760602367
yumfriez <budaevqwerty@gmail.com>
2019-02-18 13:29:03 +0300

    Project attributes tests data update

--- src/test/resources/db/migration/V001__initialize_schema.sql
+++ src/test/resources/db/migration/V001__initialize_schema.sql
@@ -1349,6 +1349,7 @@ BEGIN
     INSERT INTO attribute (name) VALUES ('analyzer.indexingRunning');
     INSERT INTO attribute (name) VALUES ('analyzer.isAutoAnalyzerEnabled');
     INSERT INTO attribute (name) VALUES ('analyzer.autoAnalyzerMode');
+    INSERT INTO attribute (name) VALUES ('notifications.enabled');
 
     -- Superadmin project and user
     INSERT INTO project (name, project_type, creation_date, metadata) VALUES ('superadmin_personal', 'PERSONAL', now(), '{"metadata": {"additional_info": ""}}');
@@ -1387,6 +1388,7 @@ BEGIN
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (9, false, defaultProject), (9, false, superadminProject);
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (10, false, defaultProject), (10, false, superadminProject);
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (11, 'LAUNCH_NAME', defaultProject), (11, 'LAUNCH_NAME', superadminProject);
+    INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (12, 'false', defaultProject), (12, 'false', superadminProject);
 
 END
 $$;
