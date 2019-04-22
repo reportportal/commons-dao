d7f0e1b5919e2358bbf795cd4dbfda859d2fe14e
yumfriez <budaevqwerty@gmail.com>
2019-04-22 20:32:54 +0300

    Custom pattern template repository extension impl

--- src/main/java/com/epam/ta/reportportal/dao/PatternTemplateRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/PatternTemplateRepository.java
@@ -23,7 +23,7 @@ import java.util.List;
 /**
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
  */
-public interface PatternTemplateRepository extends ReportPortalRepository<PatternTemplate, Long> {
+public interface PatternTemplateRepository extends ReportPortalRepository<PatternTemplate, Long>, PatternTemplateRepositoryCustom {
 
 	List<PatternTemplate> findAllByProjectId(Long projectId);
 }
