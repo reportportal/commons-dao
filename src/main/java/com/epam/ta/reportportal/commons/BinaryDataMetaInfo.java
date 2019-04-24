c0828eb14aa1b200443df4252680287bae20e471
Konstantin Antipin <konstantin_antipin@epam.com>
2019-04-24 10:45:41 +0300

    revert back BinaryDataMetaInfo

--- src/main/java/com/epam/ta/reportportal/commons/BinaryDataMetaInfo.java
+++ src/main/java/com/epam/ta/reportportal/commons/BinaryDataMetaInfo.java
@@ -22,24 +22,15 @@ public class BinaryDataMetaInfo {
 
 	private final String thumbnailFileId;
 
-	private final Long projectId;
-
-	private final String itemId;
-
-	private final String launchId;
-
 	/**
 	 * Object to hold information about saved file.
 	 *
 	 * @param fileId
 	 * @param thumbnailFileId
 	 */
-	public BinaryDataMetaInfo(String fileId, String thumbnailFileId, Long projectId, String itemId, String launchId) {
+	public BinaryDataMetaInfo(String fileId, String thumbnailFileId) {
 		this.fileId = fileId;
 		this.thumbnailFileId = thumbnailFileId;
-		this.projectId = projectId;
-		this.itemId = itemId;
-		this.launchId = launchId;
 	}
 
 	public String getFileId() {
@@ -50,24 +41,9 @@ public class BinaryDataMetaInfo {
 		return thumbnailFileId;
 	}
 
-	public Long getProjectId() {
-		return projectId;
-	}
-
-	public String getItemId() {
-		return itemId;
-	}
-
-	public String getLaunchId() {
-		return launchId;
-	}
-
 	public static final class BinaryDataMetaInfoBuilder {
 		private String fileId;
 		private String thumbnailFileId;
-		private Long projectId;
-		private String itemId;
-		private String launchId;
 
 		private BinaryDataMetaInfoBuilder() {
 		}
@@ -86,23 +62,8 @@ public class BinaryDataMetaInfo {
 			return this;
 		}
 
-		public BinaryDataMetaInfoBuilder withProjectId(Long projectId) {
-			this.projectId = projectId;
-			return this;
-		}
-
-		public BinaryDataMetaInfoBuilder withItemId(String itemId) {
-			this.itemId = itemId;
-			return this;
-		}
-
-		public BinaryDataMetaInfoBuilder withLaunchId(String launchId) {
-			this.launchId = launchId;
-			return this;
-		}
-
 		public BinaryDataMetaInfo build() {
-			return new BinaryDataMetaInfo(fileId, thumbnailFileId, projectId, itemId, launchId);
+			return new BinaryDataMetaInfo(fileId, thumbnailFileId);
 		}
 	}
 }
