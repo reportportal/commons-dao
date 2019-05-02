6a1f38de5954321ce38d90478ca7c8f828e40652
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-05-02 14:41:50 +0300

    rise cumulative launches filtering count

--- src/main/java/com/epam/ta/reportportal/dao/constant/WidgetContentRepositoryConstants.java
+++ src/main/java/com/epam/ta/reportportal/dao/constant/WidgetContentRepositoryConstants.java
@@ -100,7 +100,7 @@ public class WidgetContentRepositoryConstants {
 	public static final String FLAKY_COUNT = "flakyCount";
 
 	/*Cumulative trend widget constants*/
-	public static final Integer LAUNCHES_COUNT = 150;
+	public static final Integer LAUNCHES_COUNT = 600;
 	public static final String LAUNCHES_TABLE = "launches_table";
 	public static final String START_TIME = "start_time";
 	public static final String LATEST_NUMBER = "latest_number";
