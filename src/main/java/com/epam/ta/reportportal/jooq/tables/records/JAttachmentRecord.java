22445b944b59c5ae9f046fe33a17406c5f516a35
yumfriez <budaevqwerty@gmail.com>
2019-06-07 17:01:47 +0300

    Location field has been added to the TestItem entity

--- src/main/java/com/epam/ta/reportportal/jooq/tables/records/JAttachmentRecord.java
+++ src/main/java/com/epam/ta/reportportal/jooq/tables/records/JAttachmentRecord.java
@@ -3,14 +3,16 @@
  */
 package com.epam.ta.reportportal.jooq.tables.records;
 
+
 import com.epam.ta.reportportal.jooq.tables.JAttachment;
+
+import javax.annotation.Generated;
+
 import org.jooq.Field;
 import org.jooq.Record1;
 import org.jooq.Record7;
 import org.jooq.Row7;
 import org.jooq.impl.UpdatableRecordImpl;
-
-import javax.annotation.Generated;
 
 
 /**
