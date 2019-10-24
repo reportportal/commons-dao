777872a408723c36888eb3d2f59db9a4a599d54a
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-10-24 11:11:05 +0300

    Add find auth methods

--- src/main/java/com/epam/ta/reportportal/dao/IntegrationRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/IntegrationRepository.java
@@ -164,4 +164,10 @@ public interface IntegrationRepository extends ReportPortalRepository<Integratio
 	void updateEnabledStateByIntegrationTypeId(@Param("enabled") boolean enabled, @Param("integrationTypeId") Long integrationTypeId);
 
 	Optional<Integration> findByNameAndTypeId(String name, Long integrationTypeId);
+
+	@Query(value = "SELECT i.id, i.name, i.enabled, i.project_id, i.creator, i.creation_date, i.params, i.type FROM integration i JOIN integration_type it ON i.type = it.id WHERE i.name = :name AND it.name = :typeName AND it.group_type = 'AUTH'::INTEGRATION_GROUP_ENUM", nativeQuery = true)
+	Optional<Integration> findAuthByNameAndTypeName(@Param("name") String name, @Param("typeName") String typeName);
+
+	@Query(value = "SELECT i.id, i.name, i.enabled, i.project_id, i.creator, i.creation_date, i.params, i.type FROM integration i JOIN integration_type it ON i.type = it.id WHERE i.name = :name AND it.name = :name AND it.group_type = 'AUTH'::INTEGRATION_GROUP_ENUM", nativeQuery = true)
+	Optional<Integration> findExclusiveAuth(@Param("name") String name);
 }
