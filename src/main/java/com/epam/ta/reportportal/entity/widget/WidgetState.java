96fcb5ab95ce5a5d6cc19b62584edf555eff1342
Ivan Budayeu <ivan_budayeu@epam.com>
2020-06-24 09:52:48 +0300

    WidgetState provided

---
+++ src/main/java/com/epam/ta/reportportal/entity/widget/WidgetState.java
@@ -0,0 +1,29 @@
+package com.epam.ta.reportportal.entity.widget;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+public enum WidgetState {
+
+	RENDERING("rendering"),
+
+	READY("ready");
+
+	WidgetState(String value) {
+		this.value = value;
+	}
+
+	private final String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public static Optional<WidgetState> findByName(@Nullable String name) {
+		return Arrays.stream(WidgetState.values()).filter(state -> state.getValue().equalsIgnoreCase(name)).findAny();
+	}
+}
