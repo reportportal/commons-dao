f0ec2d70c93303c239262a8f1cdfaf27151d0f1e
Ivan_Budayeu <Skileton2018>
2021-11-03 14:38:26 +0300

    Cluster structure refactoring

--- src/test/resources/db/fill/item/items-fill.sql
+++ src/test/resources/db/fill/item/items-fill.sql
@@ -1,7 +1,7 @@
 SELECT items_init();
 
-INSERT INTO clusters(id, project_id, launch_id, message)
-VALUES (1, 1, 1, 'Message');
+INSERT INTO clusters(id, index_id, project_id, launch_id, message)
+VALUES (1, 1, 1, 1, 'Message');
 
 UPDATE log
 SET cluster_id = 1
