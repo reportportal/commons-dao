2fb1fb6167742591ece5f675190cdda80e9cc21a
Pavel_Bortnik <pavel_bortnik@epam.com>
2022-06-23 19:13:57 +0300

    EPMRPP-77972 || Return query

--- src/main/java/com/epam/ta/reportportal/dao/TestItemRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/TestItemRepository.java
@@ -343,10 +343,10 @@ public interface TestItemRepository extends ReportPortalRepository<TestItem, Lon
 	List<Long> findIdsByParentIds(@Param("parentIds") Long... parentIds);
 
 	/**
-	 * Select retries path with provided retry of
+	 * Select items ids with provided retry of
 	 * @param retryOf Retry of test item id
 	 * @return List of item ids
 	 */
-	@Query(value = "SELECT t.path FROM test_item t WHERE t.retry_of = :retryOf", nativeQuery = true)
-	List<String> findPathsByRetryOf(@Param("retryOf") Long retryOf);
+	@Query(value = "SELECT t.item_id FROM test_item t WHERE t.retry_of = :retryOf", nativeQuery = true)
+	List<Long> findIdsByRetryOf(@Param("retryOf") Long retryOf);
 }
