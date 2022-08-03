80d070c25e70a7014bab7cfcd0f8c01656b1df8f
APiankouski <109206864+APiankouski@users.noreply.github.com>
2022-08-03 19:39:15 +0300

    EPMRPP-78211 || Update DB with organizations (#837)
    Co-authored-by: Andrei Piankouski <andrei.piankouski@ukg.com>
    Co-authored-by: Pavel Bortnik <pavel_bortnik@epam.com>

---
+++ src/main/java/com/epam/ta/reportportal/entity/organization/OrganizationUserId.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.entity.organization;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * @author Andrei Piankouski
+ */
+@Embeddable
+public class OrganizationUserId implements Serializable {
+
+	@Column(name = "organization_id")
+	private Long organizationId;
+
+	@Column(name = "user_id")
+	private Long userId;
+
+	public OrganizationUserId() {
+	}
+
+	public OrganizationUserId(Long organizationId, Long userId) {
+		this.organizationId = organizationId;
+		this.userId = userId;
+	}
+
+	public Long getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(Long organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof OrganizationUserId)) {
+			return false;
+		}
+		OrganizationUserId that = (OrganizationUserId) o;
+		return Objects.equals(organizationId, that.organizationId) && Objects.equals(userId, that.userId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(organizationId, userId);
+	}
+}
