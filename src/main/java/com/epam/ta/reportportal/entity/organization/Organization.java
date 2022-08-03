41f922f1fa952dd5bf68c93e72ee0a1ddab2ab1e
APiankouski <109206864+apiankouski@users.noreply.github.com>
2022-08-03 19:39:15 +0300

    EPMRPP-78211 || Update DB with organizations (#837)
    Co-authored-by: Andrei Piankouski <andrei.piankouski@ukg.com>
    Co-authored-by: Pavel Bortnik <pavel_bortnik@epam.com>

---
+++ src/main/java/com/epam/ta/reportportal/entity/organization/Organization.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.entity.organization;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * @author Andrei Piankouski
+ */
+@Entity
+@Table(name = "organization", schema = "public")
+public class Organization implements Serializable {
+
+	private static final long serialVersionUID = 6730810629133187834L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false, precision = 64)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	public Organization() {
+	}
+
+	public Organization(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Organization)) {
+			return false;
+		}
+		Organization organization = (Organization) o;
+		return id.equals(organization.id) && name.equals(organization.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+}
