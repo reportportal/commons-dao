e3e68c0b6ea1301cdae92c1d0f680507ec705c63
Andrei Piankouski <andrei_piankouski@epam.com>
2022-08-31 15:19:57 +0300

    EPMRPP-78912 || Routing refactoring for project

--- src/main/java/com/epam/ta/reportportal/entity/project/Project.java
+++ src/main/java/com/epam/ta/reportportal/entity/project/Project.java
@@ -19,6 +19,7 @@ package com.epam.ta.reportportal.entity.project;
 import com.epam.ta.reportportal.entity.Metadata;
 import com.epam.ta.reportportal.entity.enums.ProjectType;
 import com.epam.ta.reportportal.entity.integration.Integration;
+import com.epam.ta.reportportal.entity.organization.Organization;
 import com.epam.ta.reportportal.entity.pattern.PatternTemplate;
 import com.epam.ta.reportportal.entity.project.email.SenderCase;
 import com.epam.ta.reportportal.entity.user.ProjectUser;
@@ -77,9 +78,10 @@ public class Project implements Serializable {
 	@Column(name = "metadata")
 	private Metadata metadata;
 
-	//TODO: Remove after migration
-	@Column(name = "organization")
-	private String organization;
+	@Id
+	@ManyToOne
+	@JoinColumn(name = "organization_id")
+	private Organization organization;
 
 	@Column(name = "allocated_storage", updatable = false)
 	private long allocatedStorage;
@@ -196,11 +198,11 @@ public class Project implements Serializable {
 		this.senderCases = senderCases;
 	}
 
-	public String getOrganization() {
+	public Organization getOrganization() {
 		return organization;
 	}
 
-	public void setOrganization(String organization) {
+	public void setOrganization(Organization organization) {
 		this.organization = organization;
 	}
 
@@ -229,7 +231,6 @@ public class Project implements Serializable {
 
 	@Override
 	public int hashCode() {
-
 		return Objects.hash(name, creationDate, metadata, allocatedStorage);
 	}
 
