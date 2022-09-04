98cf94582b9bf88c0c9a808226d97328481207ff
Andrei Piankouski <andrei_piankouski@epam.com>
2022-09-05 02:34:30 +0300

    EPMRPP-78912 || Routing refactoring for project

--- src/test/java/com/epam/ta/reportportal/dao/ProjectUserRepositoryTest.java
+++ src/test/java/com/epam/ta/reportportal/dao/ProjectUserRepositoryTest.java
@@ -3,7 +3,6 @@ package com.epam.ta.reportportal.dao;
 import com.epam.ta.reportportal.BaseTest;
 import com.epam.ta.reportportal.commons.ReportPortalUser;
 import com.epam.ta.reportportal.entity.project.ProjectRole;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,7 +26,7 @@ public class ProjectUserRepositoryTest extends BaseTest {
 
 		assertTrue(projectDetails.isPresent());
 
-		assertEquals(projectKey, projectDetails.get().getProjectKey());
+		assertEquals(projectKey, projectDetails.get().getProjectName());
 		assertEquals(1L, projectDetails.get().getProjectId());
 		assertEquals(ProjectRole.PROJECT_MANAGER, projectDetails.get().getProjectRole());
 	}
