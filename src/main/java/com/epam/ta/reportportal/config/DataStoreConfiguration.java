97700860e5a4ca82c9f72cba4c3b608f2169708c
Ivan_Kustau <Ivan_Kustau@epam.com>
2022-10-13 11:14:17 +0300

    EPMRPP-79211 || Change bucketToRegion implementation to use custom location

--- src/main/java/com/epam/ta/reportportal/config/DataStoreConfiguration.java
+++ src/main/java/com/epam/ta/reportportal/config/DataStoreConfiguration.java
@@ -23,23 +23,106 @@ import com.epam.reportportal.commons.TikaContentTypeResolver;
 import com.epam.ta.reportportal.filesystem.DataStore;
 import com.epam.ta.reportportal.filesystem.LocalDataStore;
 import com.epam.ta.reportportal.filesystem.distributed.s3.S3DataStore;
+import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
+import com.google.common.cache.CacheLoader;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.inject.Module;
 import org.jclouds.ContextBuilder;
+import org.jclouds.aws.s3.config.AWSS3HttpApiModule;
 import org.jclouds.blobstore.BlobStore;
 import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.ContainerNotFoundException;
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.jclouds.rest.ConfiguresHttpApi;
+import org.jclouds.s3.S3Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Set;
+
 /**
  * @author Dzianis_Shybeka
  */
 @Configuration
 public class DataStoreConfiguration {
+
+	@ConfiguresHttpApi
+	private static class CustomBucketToRegion extends AWSS3HttpApiModule {
+		private final String region;
+
+		public CustomBucketToRegion(String region){
+			this.region = region;
+		}
+
+		@Override
+		@SuppressWarnings("Guava")
+		protected CacheLoader<String, Optional<String>> bucketToRegion(Supplier<Set<String>> regionSupplier, S3Client client) {
+			Set<String> regions = regionSupplier.get();
+			if (regions.isEmpty()) {
+				return new CacheLoader<>() {
+
+					@Override
+					@SuppressWarnings({ "Guava", "NullableProblems" })
+					public Optional<String> load(String bucket) {
+						if (CustomBucketToRegion.this.region != null) {
+							return Optional.of(CustomBucketToRegion.this.region);
+						}
+						return Optional.absent();
+					}
+
+					@Override
+					public String toString() {
+						return "noRegions()";
+					}
+				};
+			} else if (regions.size() == 1) {
+				final String onlyRegion = Iterables.getOnlyElement(regions);
+				return new CacheLoader<>() {
+					@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+					final Optional<String> onlyRegionOption = Optional.of(onlyRegion);
+
+					@Override
+					@SuppressWarnings("NullableProblems")
+					public Optional<String> load(String bucket) {
+						if (CustomBucketToRegion.this.region != null) {
+							return Optional.of(CustomBucketToRegion.this.region);
+						}
+						return onlyRegionOption;
+					}
+
+					@Override
+					public String toString() {
+						return "onlyRegion(" + onlyRegion + ")";
+					}
+				};
+			} else {
+				return new CacheLoader<>() {
+					@Override
+					@SuppressWarnings("NullableProblems")
+					public Optional<String> load(String bucket) {
+						if (CustomBucketToRegion.this.region != null) {
+							return Optional.of(CustomBucketToRegion.this.region);
+						}
+						try {
+							return Optional.fromNullable(client.getBucketLocation(bucket));
+						} catch (ContainerNotFoundException e) {
+							return Optional.absent();
+						}
+					}
+
+					@Override
+					public String toString() {
+						return "bucketToRegion()";
+					}
+				};
+			}
+		}
+	}
 
 	@Bean
 	@ConditionalOnProperty(name = "datastore.type", havingValue = "filesystem")
@@ -49,9 +132,10 @@ public class DataStoreConfiguration {
 
 	@Bean
 	@ConditionalOnProperty(name = "datastore.type", havingValue = "s3")
-	public BlobStore blobStore(@Value("${datastore.s3.accessKey}") String accessKey, @Value("${datastore.s3.secretKey}") String secretKey) {
+	public BlobStore blobStore(@Value("${datastore.s3.accessKey}") String accessKey, @Value("${datastore.s3.secretKey}") String secretKey,
+			@Value("${datastore.s3.region}") String region) {
 		Iterable<Module> modules = ImmutableSet.of(
-				new SLF4JLoggingModule());
+				new SLF4JLoggingModule(), new CustomBucketToRegion(region));
 
 		BlobStoreContext blobStoreContext = ContextBuilder.newBuilder("aws-s3")
 				.modules(modules)
