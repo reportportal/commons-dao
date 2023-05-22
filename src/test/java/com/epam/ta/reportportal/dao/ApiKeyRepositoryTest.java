78d9f32a3fef2ca6719649bc8bd090a99942d10a
APiankouski <109206864+APiankouski@users.noreply.github.com>
2023-05-22 17:04:18 +0300

    EPMRPP-83130 || Update the token generation and its storage (#883)
    * EPMRPP-83130 || Update the token generation and its storage
    
    * EPMRPP-83130 || Add findByUser
    
    * EPMRPP-83130 || Add java doc
    
    ---------
    
    Co-authored-by: Andrei Piankouski <andrei_piankouski@epam.com>

---
+++ src/test/java/com/epam/ta/reportportal/dao/ApiKeyRepositoryTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.epam.ta.reportportal.BaseTest;
+import com.epam.ta.reportportal.entity.user.ApiKey;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Andrei Piankouski
+ */
+public class ApiKeyRepositoryTest extends BaseTest {
+
+  @Autowired
+  private ApiKeyRepository apiKeyRepository;
+
+  @Test
+  void shouldInsertAndSetId() {
+    final ApiKey apiKey = new ApiKey();
+    apiKey.setName("ApiKey");
+    apiKey.setHash("8743b52063cd84097a65d1633f5c74f5");
+    apiKey.setCreatedAt(LocalDateTime.now());
+    apiKey.setUserId(1L);
+
+    ApiKey saved = apiKeyRepository.save(apiKey);
+
+    assertNotNull(saved.getId());
+  }
+
+}
