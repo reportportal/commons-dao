a81b3d7c2629f31e9ab18615d3b6ed35cbf336de
Andrei Piankouski <andrei_piankouski@epam.com>
2023-08-30 17:44:47 +0300

    Revert "EPMRPP-84513 || Performance degradation during reporting after the fix to an assigned and an unassigned project"
    This reverts commit 87b97c1687c0147ac276a261a131a2d5fa578bf6.

--- src/main/java/com/epam/ta/reportportal/dao/ProjectUserRepositoryCustom.java
+++ src/main/java/com/epam/ta/reportportal/dao/ProjectUserRepositoryCustom.java
@@ -5,8 +5,5 @@ import com.epam.ta.reportportal.commons.ReportPortalUser;
 import java.util.Optional;
 
 public interface ProjectUserRepositoryCustom {
-
 	Optional<ReportPortalUser.ProjectDetails> findDetailsByUserIdAndProjectName(Long userId, String projectName);
-
-	Optional<ReportPortalUser.ProjectDetails> findAdminDetailsByProjectName(String projectName);
 }
