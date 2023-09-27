511187a0726c53e535800452e18b562a3f6bc720
Pavel Bortnik <pavel_bortnik@epam.com>
2023-09-27 13:57:38 +0300

    EPMRPP-86361 || Add possibility to filter items by composite system attributes

--- src/main/java/com/epam/ta/reportportal/commons/querygen/constant/ItemAttributeConstant.java
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/constant/ItemAttributeConstant.java
@@ -27,6 +27,7 @@ public final class ItemAttributeConstant {
   public static final String CRITERIA_ITEM_ATTRIBUTE_VALUE = "attributeValue";
   public static final String CRITERIA_ITEM_ATTRIBUTE_SYSTEM = "attributeSystem";
   public static final String CRITERIA_COMPOSITE_ATTRIBUTE = "compositeAttribute";
+  public static final String CRITERIA_COMPOSITE_SYSTEM_ATTRIBUTE = "compositeSystemAttribute";
   public static final String CRITERIA_LEVEL_ATTRIBUTE = "levelAttribute";
   public static final String KEY_VALUE_SEPARATOR = ":";
   public static final JItemAttribute LAUNCH_ATTRIBUTE = JItemAttribute.ITEM_ATTRIBUTE.as(
