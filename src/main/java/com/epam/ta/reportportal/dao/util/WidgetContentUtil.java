b2c73acfdbd0dd07bcd5f790f1e1bb61eeaa7dc3
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2024-03-04 10:41:14 +0300

    EPMRPP-89216 exclude skipped tests from HCW table view (#989)
    * EPMRPP-89216 exclude skipped tests from HCW table view

--- src/main/java/com/epam/ta/reportportal/dao/util/WidgetContentUtil.java
+++ src/main/java/com/epam/ta/reportportal/dao/util/WidgetContentUtil.java
@@ -628,14 +628,11 @@ public class WidgetContentUtil {
 
         });
 
-        resultMap.values()
-            .stream()
-            .map(content -> excludeSkippedTests(params, content))
-            .forEach(content -> {
-          double passingRate = 100.0 * content.getStatistics().getOrDefault(EXECUTIONS_PASSED, 0)
-              / content.getStatistics().getOrDefault(EXECUTIONS_TOTAL, 1);
-              content.setPassingRate(new BigDecimal(passingRate).setScale(2, RoundingMode.HALF_UP)
-                  .doubleValue());
+        resultMap.forEach((key, content) -> {
+          double passingRate = 100.0 * content.getStatistics().getOrDefault(EXECUTIONS_PASSED, 0) /
+              content.getStatistics().getOrDefault(EXECUTIONS_TOTAL, 1);
+          content.setPassingRate(BigDecimal.valueOf(passingRate)
+              .setScale(2, RoundingMode.HALF_UP).doubleValue());
         });
 
         return resultMap;
