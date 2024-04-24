56ee847505f673ab6fba9c4669e1098a61161093
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-04-24 12:22:57 +0300

    EPMRPP-90205 invitation links migration

--- src/test/resources/db/fill/user-bid/user-bid-fill.sql
+++ src/test/resources/db/fill/user-bid/user-bid-fill.sql
@@ -1,17 +1,26 @@
-INSERT INTO user_creation_bid(uuid, last_modified, email, project_name, role, metadata, inviting_user_id)
-VALUES ('0647cf8f-02e3-4acd-ba3e-f74ec9d2c5cb', now(), 'superadminemail@domain.com', 1, 'MANAGER', '{
-  "metadata": {
-    "type": "internal"
-  }
-}', 1),
+INSERT INTO organization (id, name, slug, organization_type)
+    VALUES (2, 'Invite org', 'invite-org', 'INTERNAL');
 
-       ('6cb0ce2a-e974-44d8-ae78-d86baa38c356', now() - interval '30 day', 'defaultemail@domain.com', 2, 'MANAGER', '{
-         "metadata": {
-           "type": "internal"
-         }
-       }', 1),
-       ('04ff29db-88ef-44c9-9273-5701f6969127', now() - interval '1 day', 'defaultemail@domain.com', 1, 'MEMBER', '{
-         "metadata": {
-           "type": "internal"
-         }
-       }', 1);
+INSERT INTO user_creation_bid(uuid, last_modified, email, project_name, role, organization_id,
+                              organization_role, inviting_user_id, metadata)
+VALUES ('0647cf8f-02e3-4acd-ba3e-f74ec9d2c5cb', now(), 'superadminemail@domain.com', 1, 'EDITOR',
+        2, 'MANAGER', 1,
+        '{
+          "metadata": {
+            "type": "internal"
+          }
+        }'),
+       ('6cb0ce2a-e974-44d8-ae78-d86baa38c356', now() - interval '30 day',
+        'defaultemail@domain.com', 2, 'EDITOR', 2, 'MANAGER', 1,
+        '{
+          "metadata": {
+            "type": "internal"
+          }
+        }'),
+       ('04ff29db-88ef-44c9-9273-5701f6969127', now() - interval '1 day', 'defaultemail@domain.com',
+        1, 'VIEWER', 2, 'MEMBER', 1,
+        '{
+          "metadata": {
+            "type": "internal"
+          }
+        }');
