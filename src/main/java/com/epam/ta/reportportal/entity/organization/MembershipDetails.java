64bcd9231489cd178b91adf3ccc0288c69faf115
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-05-05 22:37:37 +0300

    EPMRPP-89966 roles refactor

--- src/main/java/com/epam/ta/reportportal/entity/organization/MembershipDetails.java
+++ src/main/java/com/epam/ta/reportportal/entity/organization/MembershipDetails.java
@@ -1,5 +1,7 @@
 package com.epam.ta.reportportal.entity.organization;
 
+import com.epam.reportportal.rules.exception.ErrorType;
+import com.epam.reportportal.rules.exception.ReportPortalException;
 import com.epam.ta.reportportal.entity.project.ProjectRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,5 +21,67 @@ public class MembershipDetails {
   private String projectName;
   private String projectKey;
   private ProjectRole projectRole;
+
+  public static MembershipDetailsBuilder builder() {
+    return new MembershipDetailsBuilder();
+  }
+
+  public static class MembershipDetailsBuilder {
+
+    private Long orgId;
+    private String orgName;
+    private OrganizationRole orgRole;
+    private Long projectId;
+    private String projectName;
+    private String projectKey;
+    private ProjectRole projectRole;
+
+    private MembershipDetailsBuilder() {
+    }
+
+    public MembershipDetails.MembershipDetailsBuilder withOrgId(Long orgId) {
+      this.orgId = orgId;
+      return this;
+    }
+
+    public MembershipDetails.MembershipDetailsBuilder withOrgName(String orgName) {
+      this.orgName = orgName;
+      return this;
+    }
+
+    public MembershipDetails.MembershipDetailsBuilder withOrgRole(String orgRole) {
+      this.orgRole = OrganizationRole.forName(orgRole)
+          .orElseThrow(() -> new ReportPortalException(ErrorType.ROLE_NOT_FOUND, orgRole));
+      return this;
+    }
+
+    public MembershipDetails.MembershipDetailsBuilder withProjectId(Long projectId) {
+      this.projectId = projectId;
+      return this;
+    }
+
+    public MembershipDetails.MembershipDetailsBuilder withProjectName(String projectName) {
+      this.projectName = projectName;
+      return this;
+    }
+
+    public MembershipDetails.MembershipDetailsBuilder withProjectKey(String projectKey) {
+      this.projectKey = projectKey;
+      return this;
+    }
+
+
+    public MembershipDetails.MembershipDetailsBuilder withProjectRole(String projectRole) {
+      this.projectRole = ProjectRole.forName(projectRole)
+          .orElseThrow(() -> new ReportPortalException(ErrorType.ROLE_NOT_FOUND, projectRole));
+      return this;
+    }
+
+    public MembershipDetails build() {
+      return new MembershipDetails(orgId, orgName, orgRole, projectId, projectName, projectKey,
+          projectRole);
+    }
+  }
+
 
 }
