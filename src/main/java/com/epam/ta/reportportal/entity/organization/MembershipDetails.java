ec895aae09eff17dad92d900e331f91de4cc5eac
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-05-13 09:36:27 +0300

    EPMRPP-89966 roles refactor

--- src/main/java/com/epam/ta/reportportal/entity/organization/MembershipDetails.java
+++ src/main/java/com/epam/ta/reportportal/entity/organization/MembershipDetails.java
@@ -2,6 +2,7 @@ package com.epam.ta.reportportal.entity.organization;
 
 import com.epam.ta.reportportal.entity.project.ProjectRole;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class MembershipDetails {
 
   private Long orgId;
