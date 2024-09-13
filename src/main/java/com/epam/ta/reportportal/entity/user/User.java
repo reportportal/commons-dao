2cbb18b0680ded905dbebfba5041ba328082c13a
Pavel_Bortnik <pavel_bortnik@epam.com>
2024-09-13 14:09:51 +0300

    EPMRPP-89105, EPMRPP-89100, EPMRPP-92451 || Add active flag for user

--- src/main/java/com/epam/ta/reportportal/entity/user/User.java
+++ src/main/java/com/epam/ta/reportportal/entity/user/User.java
@@ -19,7 +19,6 @@ package com.epam.ta.reportportal.entity.user;
 import com.epam.ta.reportportal.entity.Metadata;
 import com.google.common.collect.Sets;
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import javax.persistence.CascadeType;
@@ -33,13 +32,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import org.checkerframework.checker.units.qual.C;
+import lombok.Data;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 /**
  * @author Andrei Varabyeu
  */
+@Data
 @Entity
 @TypeDef(name = "meta", typeClass = Metadata.class)
 @Table(name = "users", schema = "public")
@@ -96,140 +96,4 @@ public class User implements Serializable {
   @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = {CascadeType.PERSIST,
       CascadeType.MERGE, CascadeType.REFRESH})
   private Set<ProjectUser> projects = Sets.newHashSet();
-
-  public User() {
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public UUID getUuid() {
-    return uuid;
-  }
-
-  public String getExternalId() {
-    return externalId;
-  }
-
-  public void setExternalId(String externalId) {
-    this.externalId = externalId;
-  }
-
-  public boolean isActive() {
-    return active;
-  }
-
-  public void setActive(boolean active) {
-    this.active = active;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getLogin() {
-    return this.login;
-  }
-
-  public void setLogin(String login) {
-    this.login = login;
-  }
-
-  public String getPassword() {
-    return this.password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getEmail() {
-    return this.email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public UserRole getRole() {
-    return role;
-  }
-
-  public void setRole(UserRole role) {
-    this.role = role;
-  }
-
-  public Set<ProjectUser> getProjects() {
-    return projects;
-  }
-
-  public void setProjects(Set<ProjectUser> projects) {
-    this.projects = projects;
-  }
-
-  public String getFullName() {
-    return this.fullName;
-  }
-
-  public void setFullName(String fullName) {
-    this.fullName = fullName;
-  }
-
-  public boolean isExpired() {
-    return isExpired;
-  }
-
-  public void setExpired(boolean expired) {
-    isExpired = expired;
-  }
-
-  public String getAttachment() {
-    return attachment;
-  }
-
-  public void setAttachment(String attachment) {
-    this.attachment = attachment;
-  }
-
-  public String getAttachmentThumbnail() {
-    return attachmentThumbnail;
-  }
-
-  public void setAttachmentThumbnail(String attachmentThumbnail) {
-    this.attachmentThumbnail = attachmentThumbnail;
-  }
-
-  public UserType getUserType() {
-    return userType;
-  }
-
-  public void setUserType(UserType userType) {
-    this.userType = userType;
-  }
-
-  public Metadata getMetadata() {
-    return metadata;
-  }
-
-  public void setMetadata(Metadata metadata) {
-    this.metadata = metadata;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    User user = (User) o;
-    return Objects.equals(id, user.id) && Objects.equals(uuid, user.uuid);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, uuid);
-  }
 }
