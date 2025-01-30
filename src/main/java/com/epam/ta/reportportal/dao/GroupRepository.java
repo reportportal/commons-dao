f9f3ad79fa37e64cca522e3e98ebc6a56665762d
Reingold Shekhtel <raikbitters@gmail.com>
2025-01-30 17:20:29 +0100

    Add method GroupRepository.getUserProjectRoles for project name

--- src/main/java/com/epam/ta/reportportal/dao/GroupRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/GroupRepository.java
@@ -7,17 +7,29 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface GroupRepository extends ReportPortalRepository<Group, Long> {
-  @Query(value =
-    "SELECT DISTINCT gp.project_role "
-        + "FROM groups_projects gp "
-          + "JOIN groups_users gu "
-            + "ON gp.group_id = gu.group_id "
-              + "AND gu.user_id = :userId  "
-              + "AND gp.project_id = :projectId"
+  @Query(value = "SELECT DISTINCT gp.project_role "
+    + "FROM groups_projects gp "
+      + "JOIN groups_users gu "
+        + "ON gp.group_id = gu.group_id "
+          + "AND gu.user_id = :userId  "
+          + "AND gp.project_id = :projectId"
     , nativeQuery = true
   )
   List<ProjectRole> getUserProjectRoles(
     @Param("userId") Long userId,
     @Param("projectId") Long projectId
   );
+
+  @Query(value = "SELECT DISTINCT gp.project_role "
+      + "FROM groups_projects gp "
+        + "JOIN groups_users gu "
+          + "ON gp.group_id = gu.group_id "
+            + "AND gu.user_id = :userId "
+      + "WHERE gp.project_id = ("
+        + "SELECT p.id "
+        + "FROM project p "
+        + "WHERE p.name = :projectName)",
+    nativeQuery = true
+  )
+  List<ProjectRole> getUserProjectRoles(Long userId, String projectName);
 }
