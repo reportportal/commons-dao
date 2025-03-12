aa3d05b156b68484d6abb8efea8ef09f80b5d56e
Reingold Shekhtel <raikbitters@gmail.com>
2025-03-12 14:18:37 +0100

    Split GroupProjectRepository logic for two classes.

--- src/main/java/com/epam/ta/reportportal/dao/GroupProjectRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/GroupProjectRepository.java
@@ -16,122 +16,22 @@
 
 package com.epam.ta.reportportal.dao;
 
-import com.epam.ta.reportportal.commons.ReportPortalUser;
 import com.epam.ta.reportportal.entity.group.GroupProject;
 import com.epam.ta.reportportal.entity.group.GroupProjectId;
-import com.epam.ta.reportportal.entity.group.dto.GroupProjectDetailsRecord;
 import com.epam.ta.reportportal.entity.project.ProjectRole;
 import java.util.List;
-import java.util.Optional;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for {@link GroupProject} and related entities.
  *
  * @author <a href="mailto:Reingold_Shekhtel@epam.com">Reingold Shekhtel</a>
  * @see GroupProject
- * @see ReportPortalUser.ProjectDetails
  * @see ProjectRole
  */
 public interface GroupProjectRepository extends
     ReportPortalRepository<GroupProject, GroupProjectId> {
-
-  /**
-   * Finds all roles of the user in the project via group membership.
-   *
-   * @param userId    user id
-   * @param projectId project id
-   * @return {@link List} of {@link ProjectRole}
-   */
-  @Query(value = """
-      SELECT DISTINCT gp.project_role
-      FROM groups_projects gp
-      JOIN groups_users gu
-      ON gp.group_id = gu.group_id
-        AND gu.user_id = :userId
-      WHERE gp.project_id = :projectId
-      """,
-      nativeQuery = true
-  )
-  @Cacheable(
-      value = "groupUserProjectRolesCache",
-      key = "#userId + '_' + #projectId",
-      cacheManager = "caffeineCacheManager"
-  )
-  List<ProjectRole> findUserProjectRoles(
-      @Param("userId") Long userId,
-      @Param("projectId") Long projectId
-  );
-
-  /**
-   * Finds a raw project details of the user in the project via group membership.
-   *
-   * @param userId      user id
-   * @param projectName project name
-   * @return {@link Optional} of {@link GroupProjectDetailsRecord}
-   */
-
-  @Query(value = """
-      SELECT p.id AS projectId,
-        p.name AS projectName,
-        array_agg(gp.project_role) AS projectRole
-      FROM groups_projects gp
-      JOIN groups_users gu
-        ON gp.group_id = gu.group_id
-          AND gu.user_id = :userId
-      JOIN Project p
-        ON gp.project_id = p.id
-          AND p.name = :projectName group by p.id
-      """,
-      nativeQuery = true
-  )
-  Optional<GroupProjectDetailsRecord> findProjectDetailsRaw(
-      @Param("userId") Long userId,
-      @Param("projectName") String projectName
-  );
-
-  /**
-   * Finds project details of the user in the project via group membership.
-   *
-   * @param userId      user id
-   * @param projectName project name
-   * @return {@link Optional} of {@link ReportPortalUser.ProjectDetails}
-   */
-  @Cacheable(
-      value = "groupProjectDetailsCache",
-      key = "#userId + '_' + #projectName",
-      cacheManager = "caffeineCacheManager"
-  )
-  default Optional<ReportPortalUser.ProjectDetails> findProjectDetails(Long userId,
-      String projectName) {
-    return findProjectDetailsRaw(userId, projectName)
-        .map(record -> new ReportPortalUser.ProjectDetails(
-            record.projectId(),
-            record.projectName(),
-            record.projectRoles()
-        ));
-  }
-
-  /**
-   * Finds all projects of the user via group membership.
-   *
-   * @param userId user id
-   * @return {@link List} of {@link GroupProject}
-   */
-  @Query(value = """
-      SELECT gp.project_id, gp.group_id, gp.project_role, gp.created_at, gp.updated_at
-      FROM groups_projects gp
-      JOIN groups_users gu
-        ON gp.group_id = gu.group_id
-      WHERE gu.user_id = :user_id
-      """,
-      nativeQuery = true
-  )
-  List<GroupProject> findAllUserProjects(@Param("user_id") Long userId);
 
   /**
    * Finds all projects of the Group.
@@ -144,8 +44,8 @@ public interface GroupProjectRepository extends
   /**
    * Finds all projects of the Group with pagination.
    *
-   * @param groupId   group id
-   * @param pageable  {@link Pageable}
+   * @param groupId  group id
+   * @param pageable {@link Pageable}
    * @return {@link Page} of {@link GroupProject}
    */
   Page<GroupProject> findAllByGroupId(Long groupId, Pageable pageable);
@@ -166,4 +66,20 @@ public interface GroupProjectRepository extends
    * @return {@link List} of {@link GroupProject}
    */
   Page<GroupProject> findAllByProjectId(Long projectId, Pageable pageable);
+
+  /**
+   * Finds all groups of the Project by project name.
+   *
+   * @param projectName project name
+   * @return {@link List} of {@link GroupProject}
+   */
+  List<GroupProject> findAllByProjectName(String projectName);
+
+  /**
+   * Finds all groups of the Project by project name with pagination.
+   *
+   * @param projectName project name
+   * @return {@link List} of {@link GroupProject}
+   */
+  Page<GroupProject> findAllByProjectName(String projectName, Pageable pageable);
 }
--- src/test/java/com/epam/ta/reportportal/dao/GroupProjectRepositoryTest.java
+++ src/test/java/com/epam/ta/reportportal/dao/GroupProjectRepositoryTest.java
@@ -2,22 +2,14 @@ package com.epam.ta.reportportal.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.epam.ta.reportportal.BaseTest;
-import com.epam.ta.reportportal.commons.ReportPortalUser;
 import com.epam.ta.reportportal.entity.group.Group;
 import com.epam.ta.reportportal.entity.group.GroupProject;
-import com.epam.ta.reportportal.entity.project.Project;
-import com.epam.ta.reportportal.entity.project.ProjectRole;
-import com.epam.ta.reportportal.entity.user.User;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
@@ -26,105 +18,16 @@ import org.springframework.test.context.jdbc.Sql;
 class GroupProjectRepositoryTest extends BaseTest {
 
   @Autowired
-  private CacheManager cacheManager;
-  @Autowired
   private GroupProjectRepository groupProjectRepository;
-  @Autowired
-  private UserRepository userRepository;
-  @Autowired
-  private ProjectRepository projectRepository;
   @Autowired
   private GroupRepository groupRepository;
 
-  private User fakeChubaka;
-  private Project falcon;
   private Group rebel;
 
   @BeforeEach
   void setUp() {
-    fakeChubaka = userRepository.findByLogin("fake_chubaka")
-        .orElseThrow(() -> new RuntimeException("User not found"));
-    falcon = projectRepository.findByName("millennium_falcon")
-        .orElseThrow(() -> new RuntimeException("Project not found"));
     rebel = groupRepository.findBySlug("rebel-group")
         .orElseThrow(() -> new RuntimeException("Group not found"));
-  }
-
-  @Test
-  void shouldReturnUserGroupProjectRoles() {
-    final List<ProjectRole> projectRoles = groupProjectRepository.findUserProjectRoles(
-        fakeChubaka.getId(),
-        falcon.getId()
-    );
-    assertEquals(2, projectRoles.size());
-  }
-
-  @Test
-  void shouldGetMaxUserProjectRole() {
-    final List<ProjectRole> projectRoles = groupProjectRepository.findUserProjectRoles(
-        fakeChubaka.getId(),
-        falcon.getId()
-    );
-
-    ReportPortalUser.ProjectDetails projectDetails = new ReportPortalUser.ProjectDetails(
-        falcon.getId(),
-        falcon.getName(),
-        ProjectRole.OPERATOR
-    );
-
-    projectDetails.setHighestRole(projectRoles);
-
-    assertEquals(ProjectRole.MEMBER, projectDetails.getProjectRole());
-  }
-
-  @Test
-  void shouldReturnUserProjectDetails() {
-    Long userId = fakeChubaka.getId();
-    String projectName = falcon.getName();
-
-    final Optional<ReportPortalUser.ProjectDetails> projectDetails = groupProjectRepository
-        .findProjectDetails(userId, projectName);
-
-    assertTrue(projectDetails.isPresent());
-    assertEquals(falcon.getId(), projectDetails.get().getProjectId());
-    assertEquals(ProjectRole.MEMBER, projectDetails.get().getProjectRole());
-  }
-
-  @Test
-  void ShouldCacheUserProjectRoles() {
-    final List<ProjectRole> projectRoles = groupProjectRepository.findUserProjectRoles(
-        fakeChubaka.getId(),
-        falcon.getId()
-    );
-    assertNotNull(projectRoles);
-
-    Cache cache = cacheManager.getCache("groupUserProjectRolesCache");
-    assertNotNull(cache);
-
-    Cache.ValueWrapper valueWrapper = cache.get(fakeChubaka.getId() + "_" + falcon.getId());
-    assertNotNull(valueWrapper);
-  }
-
-  @Test
-  void ShouldCacheProjectDetails() {
-    final Long userId = fakeChubaka.getId();
-    final String projectName = falcon.getName();
-    final Optional<ReportPortalUser.ProjectDetails> projectDetails = groupProjectRepository.findProjectDetails(
-        userId,
-        projectName
-    );
-    assertTrue(projectDetails.isPresent());
-    Cache cache = cacheManager.getCache("groupProjectDetailsCache");
-    assertNotNull(cache);
-    Cache.ValueWrapper valueWrapper = cache.get(userId + "_" + projectName);
-    assertNotNull(valueWrapper);
-  }
-
-  @Test
-  void ShouldReturnAllUserProjects() {
-    List<GroupProject> groupProjects = groupProjectRepository.findAllUserProjects(
-        fakeChubaka.getId());
-    assertEquals(4, groupProjects.size());
   }
 
   @Test
