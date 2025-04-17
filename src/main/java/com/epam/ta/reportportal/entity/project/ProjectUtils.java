3dfbe89dcac205a287b43265d135d8bdbfe099fc
Reingold Shekhtel <raikbitters@gmail.com>
2025-04-17 16:51:31 +0200

    Fix regex to properly escape username in personal project check

--- src/main/java/com/epam/ta/reportportal/entity/project/ProjectUtils.java
+++ src/main/java/com/epam/ta/reportportal/entity/project/ProjectUtils.java
@@ -216,7 +216,7 @@ public class ProjectUtils {
   public static boolean isPersonalForUser(ProjectType projectType, String projectName,
       String username) {
     return projectType == ProjectType.PERSONAL && Pattern.compile(
-            LINE_START_SYMBOL + username + PERSONAL_PROJECT_POSTFIX_REGEX)
+            LINE_START_SYMBOL + Pattern.quote(username) + PERSONAL_PROJECT_POSTFIX_REGEX)
         .matcher(projectName)
         .matches();
   }
