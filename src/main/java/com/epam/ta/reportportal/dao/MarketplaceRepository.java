b1ab07dbed2e334b8473f1bdfbbc82dec266f300
Andrei Piankouski <andrei_piankouski@epam.com>
2025-07-14 14:51:55 +0300

    Add CRUD repository

---
+++ src/main/java/com/epam/ta/reportportal/dao/MarketplaceRepository.java
@@ -0,0 +1,7 @@
+package com.epam.ta.reportportal.dao;
+
+import com.epam.ta.reportportal.entity.plugin.Marketplace;
+
+public interface MarketplaceRepository extends ReportPortalRepository<Marketplace, String> {
+
+}
