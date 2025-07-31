b387012f2d2ef69804f77aaf29ee4da9afc62082
Andrei Piankouski <andrei_piankouski@epam.com>
2025-07-31 10:44:38 +0300

    EPMRPP-97023 || Get organization query optimization

--- src/main/java/com/epam/ta/reportportal/commons/querygen/FilterTarget.java
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/FilterTarget.java
@@ -1468,10 +1468,9 @@ public enum FilterTarget {
       new CriteriaHolderBuilder().newBuilder(CRITERIA_ORG_TYPE, ORGANIZATION.ORGANIZATION_TYPE,
           String.class).get(),
       new CriteriaHolderBuilder().newBuilder(CRITERIA_ORG_USERS, USERS_QUANTITY, Long.class)
-          .withAggregateCriteria(DSL.countDistinct(ORGANIZATION_USER.USER_ID).toString())
-          .get(),
-      new CriteriaHolderBuilder().newBuilder(CRITERIA_ORG_USERS, USERS_QUANTITY, Long.class)
           .withAggregateCriteria(USERS_QUANTITY)
+          .get(),
+      new CriteriaHolderBuilder().newBuilder(USERS_QUANTITY, USERS_QUANTITY, Long.class)
           .get(),
       new CriteriaHolderBuilder()
           .newBuilder(CRITERIA_ORG_USER_ID, ORGANIZATION_USER.USER_ID, Long.class)
