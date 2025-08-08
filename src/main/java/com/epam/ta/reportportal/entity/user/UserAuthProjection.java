d5fb9f1c2d01e94d75f9e4b5330d760e107f61c8
raikbitters <raikbitters@gmail.com>
2025-08-08 13:47:12 +0200

    Update copyright year in UserAuthProjection.java to 2025

--- src/main/java/com/epam/ta/reportportal/entity/user/UserAuthProjection.java
+++ src/main/java/com/epam/ta/reportportal/entity/user/UserAuthProjection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 EPAM Systems
+ * Copyright 2025 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@ import java.util.UUID;
  * @param role       User role
  * @param active     Account active status
  * @param isExpired  Account expiry status
+ *
+ * @author <a href="mailto:Reingold_Shekhtel@epam.com">Reingold Shekhtel</a>
  */
 public record UserAuthProjection(
     Long id,
