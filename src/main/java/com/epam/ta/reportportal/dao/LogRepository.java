7a82d51e44500d70e424d73a0a90c891a0fefd96
Pavel_Bortnik <pavel_bortnik@epam.com>
2025-08-13 18:18:02 +0300

    EPMRPP_106359 || Replace query with optimized one for searching nested logs of retry

--- src/main/java/com/epam/ta/reportportal/dao/LogRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/LogRepository.java
@@ -40,26 +40,36 @@ public interface LogRepository extends ReportPortalRepository<Log, Long>, LogRep
   long countLogsByLaunchId(Long launchId);
 
   @Query(value = """
-      SELECT DISTINCT
+      WITH ParentPath AS (
+          SELECT path
+          FROM test_item ti2
+          WHERE ti2.item_id = (
+              SELECT parent_id
+              FROM test_item ti3
+              WHERE ti3.item_id = :itemId
+          )
+      ),
+      TargetPath AS (
+          SELECT cast(concat(pp.path, '.', :itemId) as ltree) AS path
+          FROM ParentPath pp
+      ),
+      FilteredItems AS (
+          SELECT ti.item_id
+          FROM test_item ti
+          WHERE ti.path <@ (SELECT path FROM TargetPath)
+      )
+      SELECT
           log.id AS logId,
           log.log_level AS logLevel,
           log.log_time AS logTime,
           log.log_message AS message,
           clusters.index_id AS clusterId
-      FROM
-          log
-      LEFT JOIN clusters
-          ON log.cluster_id = clusters.id
-      WHERE
-          log.item_id IN (
-              SELECT item_id
-              FROM test_item ti
-              WHERE ti.path ~ lquery(:pathPattern)
-          )
-          AND log.log_level >= :logLevel
+      FROM log
+      LEFT JOIN clusters ON log.cluster_id = clusters.id
+      WHERE log.item_id IN (SELECT item_id FROM FilteredItems)
+        AND log.log_level >= :logLevel;
       """, nativeQuery = true)
-  List<IndexLog> findNestedLogsWithItemPathPattern(@Param("itemId") Long itemId,
-      @Param("pathPattern") String pathPattern,
+  List<IndexLog> findNestedLogsOfRetryItem(@Param("itemId") Long itemId,
       @Param("logLevel") int logLevel);
 
   @Modifying
