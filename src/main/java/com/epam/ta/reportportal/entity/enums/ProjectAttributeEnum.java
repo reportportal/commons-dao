13029f02c7aa9351961f75a2f95e9791a410e97f
Evelina Sarkisian <59954853+Evelina02@users.noreply.github.com>
2025-09-15 12:08:22 +0300

    EPMRPP-86771 || Add new notification event type (#1154)
    * EPMRPP-86771 add new notification event type
    
    * EPMRPP-86771 updade enum to include all notification types

--- src/main/java/com/epam/ta/reportportal/entity/enums/ProjectAttributeEnum.java
+++ src/main/java/com/epam/ta/reportportal/entity/enums/ProjectAttributeEnum.java
@@ -29,8 +29,10 @@ import java.util.Optional;
  */
 public enum ProjectAttributeEnum {
 
-  NOTIFICATIONS_ENABLED("notifications.enabled", String.valueOf(true)),
-  NOTIFICATIONS_EMAIL_ENABLED("notifications.email.enabled", String.valueOf(true)),
+  NOTIFICATIONS_ENABLED(Prefix.NOTIFICATIONS + "enabled", String.valueOf(true)),
+  NOTIFICATIONS_EMAIL_ENABLED(Prefix.NOTIFICATIONS + "email.enabled", String.valueOf(true)),
+  NOTIFICATIONS_TELEGRAM_ENABLED(Prefix.NOTIFICATIONS + "telegram.enabled", String.valueOf(true)),
+  NOTIFICATIONS_SLACK_ENABLED(Prefix.NOTIFICATIONS + "slack.enabled", String.valueOf(true)),
 
   INTERRUPT_JOB_TIME(Prefix.JOB + "interruptJobTime",
       String.valueOf(Duration.ofDays(1).toSeconds())),
@@ -87,6 +89,7 @@ public enum ProjectAttributeEnum {
     public static final String ANALYZER = "analyzer.";
     public static final String JOB = "job.";
     public static final String UNIQUE_ERROR = "uniqueError.";
+    public static final String NOTIFICATIONS = "notifications.";
   }
 }
 
