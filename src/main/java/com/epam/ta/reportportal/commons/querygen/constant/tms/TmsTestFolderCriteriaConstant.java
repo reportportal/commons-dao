8706d2f88c31ed8b461fb5b26ae8604d349697bb
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-09-29 13:57:15 +0300

    Add TMS infra

---
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/constant/tms/TmsTestFolderCriteriaConstant.java
@@ -0,0 +1,14 @@
+package com.epam.ta.reportportal.commons.querygen.constant.tms;
+
+public final class TmsTestFolderCriteriaConstant {
+
+  public static final String CRITERIA_TMS_TEST_FOLDER_ID = "id";
+  public static final String CRITERIA_TMS_TEST_FOLDER_NAME = "name";
+  public static final String CRITERIA_TMS_TEST_FOLDER_DESCRIPTION = "description";
+  public static final String CRITERIA_TMS_TEST_FOLDER_PARENT_ID = "parentId";
+  public static final String CRITERIA_TMS_TEST_FOLDER_PROJECT_ID = "projectId";
+
+  private TmsTestFolderCriteriaConstant() {
+    //static only
+  }
+}
---
+++ src/main/java/com/epam/ta/reportportal/commons/querygen/constant/tms/TmsTestPlanCriteriaConstant.java
@@ -0,0 +1,13 @@
+package com.epam.ta.reportportal.commons.querygen.constant.tms;
+
+public final class TmsTestPlanCriteriaConstant {
+
+  public static final String CRITERIA_TMS_TEST_PLAN_ID = "id";
+  public static final String CRITERIA_TMS_TEST_PLAN_NAME = "name";
+  public static final String CRITERIA_TMS_TEST_PLAN_DESCRIPTION = "description";
+  public static final String CRITERIA_TMS_TEST_PLAN_PROJECT_ID = "projectId";
+
+  private TmsTestPlanCriteriaConstant() {
+    //static only
+  }
+}
