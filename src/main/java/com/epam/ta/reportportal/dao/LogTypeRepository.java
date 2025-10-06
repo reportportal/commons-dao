761f7eac40a8ce392782ee39f0d94f16648a5d8c
Evelina02 <evelina02.02.2000@gmail.com>
2025-10-06 16:59:55 +0300

    EPMRPP-108032 add log type methods

--- src/main/java/com/epam/ta/reportportal/dao/LogTypeRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/LogTypeRepository.java
@@ -18,8 +18,19 @@ package com.epam.ta.reportportal.dao;
 
 import com.epam.ta.reportportal.entity.log.ProjectLogType;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LogTypeRepository extends ReportPortalRepository<ProjectLogType, Long> {
 
   List<ProjectLogType> findByProjectId(Long projectId);
+
+  @Query("SELECT COUNT(log.id) > 0 FROM ProjectLogType log " +
+      "WHERE log.projectId = :projectId AND (LOWER(log.name) = LOWER(:name) OR log.level = :level)")
+  boolean existsByProjectIdAndNameOrLevelIgnoreCase(@Param("projectId") Long projectId,
+      @Param("name") String name, @Param("level") Integer level);
+
+  @Query("SELECT COUNT(log.id) FROM ProjectLogType log " +
+      "WHERE log.projectId = :projectId AND log.filterable = true")
+  long countFilterableLogTypes(@Param("projectId") Long projectId);
 }
