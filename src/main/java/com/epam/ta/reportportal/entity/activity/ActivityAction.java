4c578ddae433b09a8f4367a9194fa2b974143f97
Evelina Sarkisian <59954853+Evelina02@users.noreply.github.com>
2025-10-28 11:28:46 +0300

    EPMRPP-108466 log type events (#1172)

--- src/main/java/com/epam/ta/reportportal/entity/activity/ActivityAction.java
+++ src/main/java/com/epam/ta/reportportal/entity/activity/ActivityAction.java
@@ -74,7 +74,11 @@ public enum ActivityAction {
   BULK_DELETE_PROJECT("bulkDeleteProject"),
   UPDATE_PATTERN_ANALYZER("updatePatternAnalysisSettings"),
 
-  UPDATE_INSTANCE("updateInstance");
+  UPDATE_INSTANCE("updateInstance"),
+
+  CREATE_LOG_TYPE("createLogType"),
+  UPDATE_LOG_TYPE("updateLogType"),
+  DELETE_LOG_TYPE("deleteLogType");
 
   private final String value;
 
