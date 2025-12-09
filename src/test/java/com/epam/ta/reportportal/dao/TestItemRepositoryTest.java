9dfee2288e88d47746ab3b10a2fcb1fb7e48a6e5
Pavel_Bortnik <pavel_bortnik@epam.com>
2025-12-09 16:25:03 +0300

    EPMRPP-110740 || Add launch id to the search query

--- src/test/java/com/epam/ta/reportportal/dao/TestItemRepositoryTest.java
+++ src/test/java/com/epam/ta/reportportal/dao/TestItemRepositoryTest.java
@@ -427,12 +427,12 @@ class TestItemRepositoryTest extends BaseTest {
 
   @Test
   void hasItemsInStatusByParent() {
-    assertTrue(testItemRepository.hasItemsInStatusByParent(2L, "1.2", StatusEnum.FAILED.name()));
+    assertTrue(testItemRepository.hasItemsInStatusByParent(2L, 1L,"1.2", StatusEnum.FAILED.name()));
   }
 
   @Test
   void hasItemsInStatusByParentNegative() {
-    assertFalse(testItemRepository.hasItemsInStatusByParent(2L, "1.2", StatusEnum.SKIPPED.name(),
+    assertFalse(testItemRepository.hasItemsInStatusByParent(2L, 1L, "1.2", StatusEnum.SKIPPED.name(),
         StatusEnum.PASSED.name()));
   }
 
