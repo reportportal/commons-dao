5f591a1ab5371598cc4214a65e16bf8b19bddaa0
Pavel_Bortnik <pavel_bortnik@epam.com>
2026-02-09 18:34:26 +0300

    EPMRPP-111871 || Update nested attachment model

--- src/main/java/com/epam/ta/reportportal/entity/item/NestedItemAttachment.java
+++ src/main/java/com/epam/ta/reportportal/entity/item/NestedItemAttachment.java
@@ -1,7 +1,9 @@
 package com.epam.ta.reportportal.entity.item;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.annotations.Type;
 
 @Data
 @AllArgsConstructor
@@ -9,7 +11,11 @@ public class NestedItemAttachment {
 
   private Long itemId;
   private String name;
+
+  @Column(name = "path", columnDefinition = "ltree")
+  @Type(com.epam.ta.reportportal.entity.LTreeType.class)
   private String path;
+
   private String fileId;
   private String fileName;
   private String contentType;
