dfe3aa8a00fad1ac776663eca6b17da35c8701ba
Pavel_Bortnik <pavel_bortnik@epam.com>
2026-02-09 15:54:48 +0300

    EPMRPP-111871 || Fix query

--- src/main/java/com/epam/ta/reportportal/entity/item/NestedItemAttachment.java
+++ src/main/java/com/epam/ta/reportportal/entity/item/NestedItemAttachment.java
@@ -1,8 +1,10 @@
 package com.epam.ta.reportportal.entity.item;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class NestedItemAttachment {
 
   private Long itemId;
