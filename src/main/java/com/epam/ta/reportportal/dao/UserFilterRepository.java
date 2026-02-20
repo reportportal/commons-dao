6ab4ffacdbac4618881f68bdfb8ce995fd1dbe49
Evelina02 <evelina02.02.2000@gmail.com>
2026-02-20 15:52:34 +0300

    Revert "EPMRPP-111882 || Find locked dashboards (#1196)"
    This reverts commit e8ca230bfeb415e986069312f03d83ce2f960e8c.

--- src/main/java/com/epam/ta/reportportal/dao/UserFilterRepository.java
+++ src/main/java/com/epam/ta/reportportal/dao/UserFilterRepository.java
@@ -20,8 +20,6 @@ import com.epam.ta.reportportal.entity.filter.UserFilter;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 /**
  * @author Pavel Bortnik
@@ -33,20 +31,23 @@ public interface UserFilterRepository extends ReportPortalRepository<UserFilter,
    * Finds filter by 'id' and 'project id'
    *
    * @param id        {@link UserFilter#id}
-   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} whose filter will be extracted
+   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} whose filter
+   *                  will be extracted
    * @return {@link UserFilter} wrapped in the {@link Optional}
    */
   Optional<UserFilter> findByIdAndProjectId(Long id, Long projectId);
 
   /**
    * @param ids       {@link Iterable} of the filter Ids
-   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} whose filters will be extracted
+   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} whose
+   *                  filters will be extracted
    * @return The {@link List} of the {@link UserFilter}
    */
   List<UserFilter> findAllByIdInAndProjectId(Collection<Long> ids, Long projectId);
 
   /**
-   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} whose filters will be extracted
+   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} whose
+   *                  filters will be extracted
    * @return The {@link List} of the {@link UserFilter}
    */
   List<UserFilter> findAllByProjectId(Long projectId);
@@ -56,8 +57,8 @@ public interface UserFilterRepository extends ReportPortalRepository<UserFilter,
    *
    * @param name      {@link UserFilter#name}
    * @param owner     {@link UserFilter#owner}
-   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} on which filter existence will
-   *                  be checked
+   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} on which
+   *                  filter existence will be checked
    * @return if exists 'true' else 'false'
    */
   boolean existsByNameAndOwnerAndProjectId(String name, String owner, Long projectId);
@@ -66,31 +67,10 @@ public interface UserFilterRepository extends ReportPortalRepository<UserFilter,
    * Checks the existence of the {@link UserFilter} with specified name on a project
    *
    * @param name      {@link UserFilter#name}
-   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} on which filter existence will
-   *                  be checked
+   * @param projectId Id of the {@link com.epam.ta.reportportal.entity.project.Project} on which
+   *                  filter existence will be checked
    * @return if exists 'true' else 'false'
    */
   boolean existsByNameAndProjectId(String name, Long projectId);
 
-  /**
-   * Finds locked dashboards that use the specified filter. Returns dashboard names.
-   *
-   * @param filterId  The filter ID
-   * @param projectId The project ID to filter dashboards
-   * @return List of dashboard names
-   */
-  @Query(value = """
-      SELECT d.name
-      FROM dashboard d
-      INNER JOIN owned_entity oe ON d.id = oe.id
-      WHERE oe.locked = true
-        AND oe.project_id = :projectId
-        AND d.id IN (
-          SELECT dw.dashboard_id
-          FROM dashboard_widget dw
-          INNER JOIN widget_filter wf ON dw.widget_id = wf.widget_id
-          WHERE wf.filter_id = :filterId
-        )
-      """, nativeQuery = true)
-  List<String> findLockedDashboardsByFilterId(@Param("filterId") Long filterId, @Param("projectId") Long projectId);
 }
